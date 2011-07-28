4c047dbc8a8255fd29b2ff89998b9861e0321ca1
Adam R. Smith <a2smith@ucsd.edu>
2011-07-28 09:11:14 -0700

    updated dependencies

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(  name = 'ioncore_r2',
             'pika==0.9.5',
             'httplib2==0.7.1',
             'pyzmq==2.1.7',
-            'gevent_zeromq==0.2.0'
+            'gevent_zeromq==0.2.0',
+            'HTTP4Store==0.3.1'
         ],
      )
