8c7d0dd3f7d8885ba249b06e1464781b1e92add7
Adam R. Smith <adam@adamia.com>
2011-08-19 09:18:13 -0700

    fix

--- prototype/service.py
+++ prototype/service.py
@@ -4,9 +4,14 @@ __author__ = 'Adam R. Smith'
 __license__ = 'Apache 2.0'
 
 from anode.base import log
+from anode.service.service import BaseService
 from interface.services.sample_service import ISampleService
 
-class SampleService(ISampleService):
+from zope.interface import implements
+
+class SampleService(BaseService):
+    implements(ISampleService)
+
     def doc_fields_(name='text_', time='datetime_', an_int='int_', a_float='float_', a_string='text_', none='???', a_dict='dict_', a_list='list_'):
         pass
 
