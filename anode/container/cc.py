58a1b29c7ab606816c32b4266b71b6d90908d71c
Thomas Lennan <thomaslennan@gmail.com>
2011-09-16 10:13:03 -0700

    Most important change is addition of dependency injection

--- anode/container/cc.py
+++ anode/container/cc.py
@@ -12,10 +12,14 @@ actual handlers functional.
 __author__ = 'Adam R. Smith'
 __license__ = 'Apache 2.0'
 
+from zope.interface import providedBy
+
 from anode.base import CFG, SERVICE_CFG, messaging, channel, GreenProcessSupervisor
 from anode.net import entity
+from anode.service.service import add_service_by_name, get_service_by_name
 
-service_cls_by_name = {}
+from anode.util.log import log
+from anode.util.containers import DotDict
 
 class Container(object):
     """
@@ -26,44 +30,111 @@ class Container(object):
     def __init__(self, *args, **kwargs):
         self.proc_sup = GreenProcessSupervisor()
 
-    def start(self):
+    def start(self, server=True):
+        log.debug("In Container.start")
+        log.debug("server: %s" % str(server))
         self.proc_sup.start() 
         self.node, self.ioloop = messaging.makeNode() # shortcut hack
         self.proc_sup.spawn('green', self.ioloop.join)
 
-        # Read the config file and start services defined there
-        # TODO likely should be done elsewhere
-        serviceNames = self.readConfig()
+        if server == True:
+            # Read the config file and start services defined there
+            # TODO likely should be done elsewhere
+            serviceNames = self.readConfig()
+            log.debug("serviceNames: %s" % str(serviceNames))
 
-        # Iterate over service name list, starting services
-        # TODO likely should be done elsewhere
-        for serviceName in serviceNames:
-            self.start_service(serviceName)
+            # Iterate over service name list, starting services
+            for serviceName in serviceNames:
+                log.debug("serviceName: %s" % str(serviceName))
+                self.start_service(serviceName)
+
+    def readConfig(self):
+        log.debug("In Container.readConfig")
+        # Loop through configured services and start them
+        services = SERVICE_CFG['apps']
+        # Return value.  Will contain list of
+        # service names from the config file
+        serviceNames = []
+        for serviceDef in services:
+            log.debug("serviceDef: %s" % str(serviceDef))
+            name = serviceDef["name"]
+
+            configParams = {}
+            if serviceDef.has_key("config"):
+                configParams = serviceDef["config"]
+
+            # Service is described in processapp tuple
+            # Field 1 is the module name
+            # Field 2 is the class name
+            module = serviceDef["processapp"][1]
+            cls = serviceDef["processapp"][2]
+
+            serviceInstance = self.forname(module, cls, configParams)
+
+            # Inject dependencies
+            serviceInstance.clients = DotDict()
+            for dependency in serviceInstance.dependencies:
+                dependencyService = get_service_by_name(dependency)
+                dependencyInterface = list(providedBy(dependencyService))[0]
+
+                client = entity.RPCClientEntityFromInterface(dependencyInterface)
+                self.start_client(dependency, client)
+                serviceInstance.clients[dependency] = client
+
+            # Add to global dict
+            add_service_by_name(name, serviceInstance)
+
+            serviceNames.append(name)
+
+        return serviceNames
+
+    def forname(self, modpath, classname, configParams={}):
+        ''' Returns a class of "classname" from module "modname". '''
+        log.debug("In Container.forname")
+        log.debug("modpath: %s" % modpath)
+        log.debug("classname: %s" % classname)
+        log.debug("configParams: %s" % str(configParams))
+        firstTime = True
+        module = __import__(modpath, fromlist=[classname])
+        classobj = getattr(module, classname)
+        return classobj(configParams)
+
 
     def stop(self):
+        log.debug("In Container.stop")
         # TODO: Have a choice of shutdown behaviors for waiting on children, timeouts, etc
         self.proc_sup.shutdown(CFG.cc.timeout.shutdown)
 
     def start_service(self, name):
-        self.start_server(name, entity.RPCEntityFromService(service_cls_by_name[name]))
+        log.debug("In Container.start_service")
+        log.debug("name: %s" % name)
+        self.start_server(name, entity.RPCEntityFromService(get_service_by_name(name)))
 
     def start_server(self, name, entity):
         """
         Start a new request/response server using the given entity as the
         handler/service
         """
+        log.debug("In Container.start_server")
+        log.debug("name: %s" % str(name))
         def generic_server(ch, entity):
+            log.debug("In generic_server. Binding name: %s" % str(name))
             ch.bind(('amq.direct', name))
             ch.listen()
             while True:
+                log.debug("service: %s blocking waiting for message" % str(name))
                 req_chan = ch.accept()
                 msg = req_chan.recv()
+                log.debug("service %s received message: %s" % (str(name),str(msg)))
                 entity.message_received(req_chan, msg)
 
         ch = self.node.channel(channel.Bidirectional)
         self.proc_sup.spawn('green', generic_server, ch, entity)
 
     def start_client(self, name, entity):
+        log.debug("In Container.start_client")
+        log.debug("name: %s" % str(name))
+        log.debug("entity: %s" % str(entity))
         ch = self.node.channel(channel.BidirectionalClient)
         ch.connect(('amq.direct', name))
         entity.attach_channel(ch)
@@ -74,7 +145,11 @@ class Container(object):
         self.proc_sup.spawn('green', client_recv, ch, entity)
 
     def start_subscriber(self, name, entity):
+        log.debug("In Container.start_subscriber")
+        log.debug("name: %s" % str(name))
+        log.debug("entity: %s" % str(entity))
         def generic_consumer(ch, entity):
+            log.debug("In generic_consumer. Binding name: %s" % str(name))
             ch.bind(('amq.direct', name))
             ch.listen()
             while True:
@@ -85,36 +160,8 @@ class Container(object):
         self.proc_sup.spawn('green', generic_consumer, ch, entity)
 
     def serve_forever(self):
+        log.debug("In Container.serve_forever")
         if not self.proc_sup.running:
             #self.start()
             pass
         self.proc_sup.join_children()
-
-    def readConfig(self):
-        # Loop through configured services and start them
-        services = SERVICE_CFG['apps']
-        # Return value.  Will contain list of
-        # service names from the config file
-        serviceNames = []
-        for serviceDef in services:
-            name = serviceDef["name"]
-
-            # Service is described in processapp tuple
-            # Field 1 is the module name
-            # Field 2 is the class name
-            module = serviceDef["processapp"][1]
-            cls = serviceDef["processapp"][2]
-
-            clsInstance = self.forname(module, cls)
-            service_cls_by_name[name] = clsInstance
-
-            serviceNames.append(name)
-        return serviceNames
-
-    def forname(self, modpath, classname):
-        ''' Returns a class of "classname" from module "modname". '''
-        firstTime = True
-        module = __import__(modpath, fromlist=[classname])
-        classobj = getattr(module, classname)
-        return classobj()
-
