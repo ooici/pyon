86af410c47e9311e713dd2ed90ba15e9ded5bb6e
Thomas Lennan <thomaslennan@gmail.com>
2011-10-12 11:27:06 -0700

    Added association methods.  Updated init logic to reflect new config concept

--- prototype/datastore_service.py
+++ prototype/datastore_service.py
@@ -3,29 +3,26 @@
 __author__ = 'Thomas R. Lennan'
 __license__ = 'Apache 2.0'
 
+from pyon.public import CFG, log
 from pyon.core.exception import NotFound
 from pyon.datastore.couchdb.couchdb_datastore import CouchDB_DataStore
 from pyon.datastore.mockdb.mockdb_datastore import MockDB_DataStore
 from interface.services.idatastore_service import BaseDatastoreService
 
-from pyon.util.log import log
-
 class DataStoreService(BaseDatastoreService):
 
-    def __init__(self, config_params={}):
-        if config_params.has_key('type'):
-            if config_params["type"] == 'CouchDB':
-                self.datastore = CouchDB_DataStore()
-                if config_params.has_key('forceClean'):
+    def service_init(self):
+        if self.CFG["type"] == 'CouchDB':
+            self.datastore = CouchDB_DataStore()
+            if 'forceClean' in self.CFG:
+                if self.CFG['forceClean'] == True:
                     try:
                         self.datastore.delete_datastore()
                     except NotFound:
                         pass
                     self.datastore.create_datastore()
-            else:
-                self.datastore = MockDB_DataStore()
         else:
-            self.datastore = MockDB_DataStore()
+             self.datastore = MockDB_DataStore()
 
     def create_datastore(self, datastore_name=''):
         return self.datastore.create_datastore(datastore_name)
@@ -69,8 +66,20 @@ class DataStoreService(BaseDatastoreService):
     def delete_doc(self, object={}, datastore_name=''):
         return self.datastore.delete_doc(object, datastore_name)
 
-    def find(self, type='', key='', key_value='', datastore_name=''):
-        return self.datastore.find(type, key, key_value, datastore_name)
+    def find(self, criteria=[], datastore_name=''):
+        return self.datastore.find(criteria, datastore_name)
 
-    def find_doc(self, type='', key='', key_value='', datastore_name=''):
-        return self.datastore.find_doc(type, key, key_value, datastore_name)
+    def find_doc(self, criteria=[], datastore_name=''):
+        return self.datastore.find_doc(criteria, datastore_name)
+
+    def find_by_association(self, criteria=[], association="", datastore_name=""):
+        return self.datastore.find_by_association(criteria, association, datastore_name)
+
+    def find_by_association_doc(self, criteria=[], association="", datastore_name=""):
+        return self.datastore.find_by_association_doc(criteria, association, datastore_name)
+
+    def resolve_association(self, subject="", predicate="", object="", datastore_name=""):
+        return self.datastore.resolve_association(tuple, datastore_name)
+
+    def resolve_association_doc(self, subject="", predicate="", object="", datastore_name=""):
+        return self.datastore.resolve_association_doc(tuple, datastore_name)
