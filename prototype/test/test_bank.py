d7adbfb477c45298a20af755d01526e9cc73cde5
Thomas Lennan <thomaslennan@gmail.com>
2011-10-12 11:28:41 -0700

    Updated client to use new RPCClient class

--- prototype/test/test_bank.py
+++ prototype/test/test_bank.py
@@ -5,38 +5,40 @@ __license__ = 'Apache 2.0'
 
 import unittest
 
-from anode.net import entity
-from anode.container import cc
+from pyon.container import cc
+from pyon.net.endpoint import RPCClient
 
 from interface.services.ibank_service import IBankService, BaseBankService
 
-#class Test_Bank(unittest.TestCase):
-#
-#    def test_bank(self):
-#        container = cc.Container()
-#        container.start() # :(
-#
-#        client = entity.RPCClientEntityFromInterface(IBankService)
-#
-#        print "Before start client"
-#        container.start_client('bank', client)
-#
-#        print "Before new account"
-#        acctNum = client.new_account('kurt', 'Savings')
-#        print "New account number: " + str(acctNum)
-#        print "Starting balance %s" % str(client.get_balance(acctNum))
-#        client.deposit(acctNum, 99999999)
-#        print "Confirming balance after deposit %s" % str(client.get_balance(acctNum))
-#        client.withdraw(acctNum, 1000)
-#        print "Confirming balance after withdrawl %s" % str(client.get_balance(acctNum))
-#        acctList = client.list_accounts('kurt')
-#        for acct_obj in acctList:
-#            print "Account: " + str(acct_obj)
-#
-#        container.stop()
+class Test_Bank(unittest.TestCase):
 
-#    def test_persistent(self):
-#        self.do_test(BankService(persistent=True))
+    def test_bank(self):
+        """
+        This is an all inclusive test in which the
+        client and server are all running within the
+        same container
+        """
+        container = cc.Container()
+        container.start() # :(
+
+        client = RPCClient(node=container.node, name="bank", iface=IBankService)
+
+        print "Before start client"
+        container.start_client('bank', client)
+
+        print "Before new account"
+        acctNum = client.new_account('kurt', 'Savings')
+        print "New account number: " + str(acctNum)
+        print "Starting balance %s" % str(client.get_balance(acctNum))
+        client.deposit(acctNum, 99999999)
+        print "Confirming balance after deposit %s" % str(client.get_balance(acctNum))
+        client.withdraw(acctNum, 1000)
+        print "Confirming balance after withdrawl %s" % str(client.get_balance(acctNum))
+        acctList = client.list_accounts('kurt')
+        for acct_obj in acctList:
+            print "Account: " + str(acct_obj)
+
+        container.stop()
 
 if __name__ == "__main__":
     unittest.main()
