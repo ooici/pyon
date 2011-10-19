6f6d0d3016ccf7183028dc555ccd00c3defaa809
Thomas Lennan <thomaslennan@gmail.com>
2011-10-19 07:42:47 -0700

    Refactored container to separate container startup and service init

--- pyon/container/cc.py
+++ pyon/container/cc.py
@@ -25,8 +25,21 @@ from pyon.util.containers import DictModifier, DotDict
 from pyon.ion.process import IonProcessSupervisor
 
 from zope.interface import providedBy
+from zope.interface import Interface, implements
+
+class IContainerAgent(Interface):
+
+    def start_app(processapp=[], config={}):
+        pass
+
+    def start_rel(rel={}):
+        pass
+
+    def start_rel_from_url(rel_url=""):
+        pass
 
 class Container(object):
+    implements(IContainerAgent)
     """
     The Capability Container. Its purpose is to spawn/monitor processes and services
     that do the bulk of the work in the ION system.
@@ -38,124 +51,88 @@ class Container(object):
         # Keep track of the overrides from the command-line, so they can trump app/rel file data
         self.spawn_args = DictModifier(CFG, kwargs)
 
-    def start(self, server=True):
+    def start(self):
         log.debug("In Container.start")
-        log.debug("server: %s" % str(server))
-        self.proc_sup.start() 
+        self.proc_sup.start()
         self.node, self.ioloop = messaging.makeNode() # shortcut hack
         self.proc_sup.spawn(('green', self.ioloop.join))
 
-    def start_rel(self, rel_file):
-        # Read the config file and start services defined there.
-        # Saving the current rel block in CFG.deploy, not sure I like this. - Adam
+    def start_app(self, processapp=[], config={}):
+        log.debug("In Container.start_app processapp: %s config: %s"%(str(processapp),str(config)))
+        # Start process defined by processapp with specified config
+        name, module, cls = processapp
 
-        rel_data = Config([rel_file]).data
-        CFG.deploy = rel_data.deploy
+        service_instance = self._for_name(module, cls)
 
-        service_names = self.read_config()
-        log.debug("service_names: %s" % str(service_names))
+        # Inject dependencies
+        service_instance.clients = DotDict()
+        log.debug("In Container.start_app dependencies: %s"%str(service_instance.dependencies))
+        for dependency in service_instance.dependencies:
+            dependency_service = get_service_by_name(dependency)
+            dependency_interface = list(providedBy(dependency_service))[0]
 
-        # Iterate over service name list, starting services
-        for serviceName in service_names:
-            log.debug("serviceName: %s" % str(serviceName))
-            self.start_service(serviceName)
+            # @TODO: start_client call instead?
+            client = RPCClient(node=self.node, name=dependency, iface=dependency_interface)
+            service_instance.clients[dependency] = client
 
-    def read_config(self):
-        log.debug("In Container.readConfig")
-        # Loop through configured services and start them
-        services = CFG.deploy.apps
-        # Return value.  Will contain list of
-        # service names from the config file
-        service_names = []
-        for service_def in services:
-            name = service_def.name
+        # Init service
+        service_instance.CFG = config
+        service_instance.service_init()
 
-            # TODO fix this
-            # For now service is described in processapp tuple in rel file
-            # Field 1 is the module name
-            # Field 2 is the class name
-            module, cls = service_def.processapp
+        # Add to global dict
+        add_service_by_name(name, service_instance)
 
-            service_instance = self.forname(module, cls)
+        rsvc = RPCServer(node=self.node, name=name, service=service_instance)
 
-            # Inject dependencies
-            service_instance.clients = DotDict()
-            for dependency in service_instance.dependencies:
-                dependency_service = get_service_by_name(dependency)
-                dependency_interface = list(providedBy(dependency_service))[0]
+        # Start an ION process with the right kind of endpoint factory
+        listener = BinderListener(self.node, name, rsvc, None, None)
+        self.proc_sup.spawn((CFG.cc.proctype or 'green', None), listener=listener)
 
-                # @TODO: start_client call instead?
-                client = RPCClient(node=self.node, name=dependency, iface=dependency_interface)
-                service_instance.clients[dependency] = client
+        return listener.get_ready_event()
 
-            # Call method to blend config values
-            service_config = self.get_service_config(name)
-            service_instance.CFG = service_config
+    def start_rel(self, rel={}):
+        # Recurse over the rel and start apps defined there.
+        log.debug("In Container.start_rel  rel: %s" % str(rel))
 
-            # Call method to allow service to self-init
-            service_instance.service_init()
+        for rel_app_cfg in rel.apps:
+            name = rel_app_cfg.name
+            log.debug("rel definition: %s" % str(rel_app_cfg))
 
-            # Add to global dict
-            add_service_by_name(name, service_instance)
+            app_file_path = ['res/apps/' + name + '.app']
+            app_file_cfg = Config(app_file_path).data
+            log.debug("app file definition: %s" % str(app_file_cfg))
 
-            service_names.append(name)
+            # Overlay app file and rel config as appropriate
+            config = DictModifier(CFG)
+            if 'config' in app_file_cfg:
+                # Apply config from app file
+                app_file_cfg = DotDict(app_file_cfg.config)
+                config.update(app_file_cfg)
 
-        return service_names
+            if 'config' in rel_app_cfg:
+                # Nest dict modifier and apply config from rel file
+                config = DictModifier(config, rel_app_cfg.config)
 
-    def forname(self, modpath, classname):
-        ''' Returns a class of "classname" from module "modname". '''
-        log.debug("In Container.forname")
-        log.debug("modpath: %s" % modpath)
-        log.debug("classname: %s" % classname)
-        module = __import__(modpath, fromlist=[classname])
-        classobj = getattr(module, classname)
-        return classobj()
+            if 'processapp' in rel_app_cfg:
+                processapp = rel_app_cfg.processapp
+            else:
+                processapp = app_file_cfg.processapp
 
-    def get_service_config(self, appname):
-        # Base is global config
-        cfg_dict = DictModifier(CFG)
+            self.start_app(processapp, config)
 
-        app_path = ['res/apps/' + appname + '.app']
-        app_cfg = Config(app_path).data
-        if 'config' in app_cfg:
-            # Apply config from app file
-            app_cfg_dict = DotDict(app_cfg.config)
-            cfg_dict.update(app_cfg_dict)
-
-        # Find service definition in rel file
-        for app_def in CFG.deploy.apps:
-            if app_def.name == appname:
-                rel_def = app_def
-                break
-        if 'config' in rel_def:
-            # Nest dict modifier and apply config from rel file
-            cfg_dict = DictModifier(cfg_dict, rel_def.config)
-
-        if self.spawn_args:
-            # Nest dict modifier and apply config from spawn args
-            cfg_dict = DictModifier(cfg_dict, self.spawn_args)
-
-        return cfg_dict
+    def start_rel_from_url(self, rel_url=""):
+        # Read the rel file and call start_rel
+        log.debug("In Container.start_rel_from_url  rel_url: %s" % str(rel_url))
+        rel = Config([rel_url]).data
+        self.start_rel(rel)
 
     def stop(self):
         log.debug("In Container.stop")
         # TODO: Have a choice of shutdown behaviors for waiting on children, timeouts, etc
         self.proc_sup.shutdown(CFG.cc.timeout.shutdown)
 
-    def start_service(self, name):
-        log.debug("In Container.start_service")
-        log.debug("name: %s" % name)
-
-        svc = get_service_by_name(name)
-        rsvc = RPCServer(node=self.node, name=name, service=svc)
-
-        # Start an ION process with the right kind of endpoint factory
-        listener = BinderListener(self.node, name, rsvc, None, None)
-        self.proc_sup.spawn((CFG.cc.proctype or 'green', None), listener=listener)
-
     def serve_forever(self):
         """ Run the container until killed. """
-        
         log.debug("In Container.serve_forever")
         
         if not self.proc_sup.running:
@@ -170,3 +147,11 @@ class Container(object):
 
         self.proc_sup.shutdown(CFG.cc.timeout.shutdown)
             
+    def _for_name(self, modpath, classname):
+        ''' Returns a class of "classname" from module "modname". '''
+        log.debug("In Container.forname")
+        log.debug("modpath: %s" % modpath)
+        log.debug("classname: %s" % classname)
+        module = __import__(modpath, fromlist=[classname])
+        classobj = getattr(module, classname)
+        return classobj()
