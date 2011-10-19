ea154efd2cb330789a68f8d86c847eac44338a0a
Dave Foster <dfoster@asascience.com>
2011-10-19 17:58:36 -0400

    Use unique name for container agent

--- pyon/container/cc.py
+++ pyon/container/cc.py
@@ -9,13 +9,14 @@ per request. This will also facilitate the Endpoint holding 'business'
 objects/resources that each request has access to. This will keep the
 actual handlers functional. 
 """
+import os
 from pyon.net.endpoint import RPCServer, RPCClient, BinderListener
 
 __author__ = 'Adam R. Smith'
 __license__ = 'Apache 2.0'
 
 from pyon.net import messaging, channel
-from pyon.core.bootstrap import CFG
+from pyon.core.bootstrap import CFG, sys_name
 from pyon.service.service import add_service_by_name, get_service_by_name
 
 from pyon.util.config import Config
@@ -45,7 +46,7 @@ class Container(object):
     that do the bulk of the work in the ION system.
     """
     node = None
-    name = "container_agent"
+    name = "container_agent-%s-%d" % (sys_name, os.getpid())
     def __init__(self, *args, **kwargs):
         log.debug("Container.__init__")
         self.proc_sup = IonProcessSupervisor(heartbeat_secs=CFG.cc.timeout.heartbeat)
