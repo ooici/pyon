c6ad2c14b98798423e01ed06dba9d203dce06698
Thomas Lennan <thomaslennan@gmail.com>
2011-10-26 16:26:46 -0700

    Changed base test class to only load repository once

--- pyon/test/pyontest.py
+++ pyon/test/pyontest.py
@@ -2,11 +2,12 @@ import unittest
 
 from pyon.core.bootstrap import obj_registry, populate_registry
 
+populate_registry()
+
 class PyonTestCase(unittest.TestCase):
     """
     Base test class to allow operations such as starting the container
     """
 
     def run(self, result=None):
-        populate_registry()
         unittest.TestCase.run(self, result)
