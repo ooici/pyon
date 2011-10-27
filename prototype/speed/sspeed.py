05b113aefd0741250c4834d559b2d19f59d7f92d
Dave Foster <dfoster@asascience.com>
2011-10-27 16:31:32 -0400

    Raw pubsub speed tests

---
+++ prototype/speed/sspeed.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from pyon.net.endpoint import Publisher, PublisherEndpoint
+from pyon.net.messaging import makeNode
+import gevent
+import time
+
+class SpeedPublisherEndpoint(PublisherEndpoint):
+    def _build_msg(self, raw_msg):
+        return raw_msg
+
+class SpeedPublisher(Publisher):
+    endpoint_type = SpeedPublisherEndpoint
+
+node,iowat=makeNode()
+pub=Publisher(node=node, name="hassan")
+#pub=SpeedPublisher(node=node, name="hassan")
+
+counter = 0
+st = time.time()
+
+def tick():
+    global counter, st
+    while True:
+        time.sleep(2)
+        ct = time.time()
+        elapsed_s = ct - st
+
+        mps = counter / elapsed_s
+
+        print counter, "messages, per sec:", mps
+
+def work():
+    global counter
+    while True:
+        pub.publish(str(counter))
+        counter += 1
+
+_gt = gevent.spawn(tick)
+_gw = gevent.spawn(work)
+
+gevent.joinall([_gt, _gw])
+
