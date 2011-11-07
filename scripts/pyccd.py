0acb718e07f75b3d3d0080aac5e8b37973c6f0c4
Stephen Henrie <shenrie@ucsd.edu>
2011-11-07 13:15:53 -0800

    Changed debugging entry point from main for use in serice repositories

--- scripts/pyccd.py
+++ scripts/pyccd.py
@@ -3,6 +3,10 @@ __author__ = 'sphenrie'
 from scripts.pycc import entry
 from pydev import pydevd
 
-if __name__ == '__main__':
+def start_debugging():
     pydevd.settrace('localhost', port=8585, stdoutToServer=True, stderrToServer=True, suspend=False)
     entry()
+   
+if __name__ == '__main__':
+    start_debugging()
+
