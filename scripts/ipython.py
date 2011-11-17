eccd48e703376533ac0313d3493febc0ec66af39
Michael Meisinger <mmeisinger@ucsd.edu>
2011-11-17 12:52:59 -0800

    Added ipython script for buildout reasons

---
+++ scripts/ipython.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Entrypoint to start ipython with buildout path
+
+__author__ = 'Michael Meisinger'
+__license__ = 'Apache 2.0'
+
+def main():
+    import sys
+    from pkg_resources import load_entry_point
+
+    sys.exit(
+       load_entry_point('ipython==0.11', 'console_scripts', 'ipython')()
+    )
+
+if __name__ == '__main__':
+    main()
