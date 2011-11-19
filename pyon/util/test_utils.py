575bb3366aa4a63adfb53ec146f94db7ba65976f
Jamie Chen <jachen@ucsd.edu>
2011-11-18 16:22:24 -0800

    add to test utils

--- pyon/util/test_utils.py
+++ pyon/util/test_utils.py
@@ -13,3 +13,14 @@ def switch_ref(module_name, attribute_name):
     module.__dict__[attribute_name] = mock
     yield mock
     module.__dict__[attribute_name] = real
+
+def  pop_last_call(mock):
+    if not mock.call_count:
+        raise AssertionError('Cannot pop last call: call_count is 0')
+    mock.call_args_list.pop()
+    try:
+        mock.call_args = mock.call_args_list[-1]
+    except IndexError:
+        mock.call_args = None
+        mock.called = False
+    mock.call_count -= 1
