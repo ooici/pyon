90b63929f3d169bd9d630f37279aa1291349796c
Stephen Henrie <shenrie@ucsd.edu>
2011-12-23 12:11:36 -0800

    Adding an example hello service client

---
+++ examples/hello_client.py
@@ -0,0 +1,30 @@
+
+__author__ = 'sphenrie'
+
+
+from pyon.public import Container
+from pyon.net.endpoint import ProcessRPCClient
+from pyon.util.context import LocalContextMixin
+
+from interface.services.examples.hello.ihello_service  import HelloServiceProcessClient
+
+class FakeProcess(LocalContextMixin):
+    name = ''
+
+def hello_client(container, user_id='anonymous', org_id='no-ooi'):
+
+   # client = ProcessRPCClient(node=container.node, name="hello", iface=IHelloService,  process=FakeProcess())
+    client = HelloServiceProcessClient(node=container.node, process=FakeProcess())
+
+    ret = client.hello(" initial text sent is 123 ", headers={'ion-user-id': user_id, 'ion-org-id': org_id})
+
+    print "Returned: " + str(ret)
+    
+
+
+if __name__ == '__main__':
+
+    container = Container()
+    container.start() # :(
+    hello_client(container, user_id='shenrie',org_id='ooi')
+    container.stop()
