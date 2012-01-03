38969b0b5d426ad1896532d7618c1f2269cb1f80
Thomas Lennan <thomaslennan@gmail.com>
2012-01-03 13:48:13 -0800

    Added hack to work around msgpack issue where msgpack is incorrectly deserializing lists as tuples.  See http://jira.msgpack.org/browse/MSGPACK-15

--- pyon/core/interceptor/codec.py
+++ pyon/core/interceptor/codec.py
@@ -1,6 +1,6 @@
 from pyon.core.interceptor.interceptor import Interceptor
 from pyon.core.bootstrap import obj_registry
-from pyon.core.object import IonObjectDeserializer, IonObjectSerializer
+from pyon.core.object import IonObjectDeserializer, IonObjectSerializer, walk
 from pyon.util.log import log
 
 class CodecInterceptor(Interceptor):
@@ -24,8 +24,20 @@ class CodecInterceptor(Interceptor):
     def incoming(self, invocation):
         log.debug("CodecInterceptor.incoming: %s", invocation)
 
-        log.debug("Payload, pre-transform: %s", invocation.message)
-        invocation.message = self._io_deserializer.deserialize(invocation.message)
+        payload = invocation.message
+        log.debug("Payload, pre-transform: %s", payload)
+
+        # Horrible, hacky workaround for msgpack issue
+        # See http://jira.msgpack.org/browse/MSGPACK-15
+        def convert_tuples_to_lists(obj):
+            if isinstance(obj, tuple):
+                res = list(obj)
+                return res
+            return obj
+
+        payload = walk(payload, convert_tuples_to_lists)
+
+        invocation.message = self._io_deserializer.deserialize(payload)
         log.debug("Payload, post-transform: %s", invocation.message)
 
         return invocation
