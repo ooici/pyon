3b014f0d30a0aa693bfa7fd86d6158547cab232d
Michael Meisinger <mmeisinger@ucsd.edu>
2012-01-05 17:43:40 -0800

    Resource lifecycle refined

--- pyon/ion/resource.py
+++ pyon/ion/resource.py
@@ -22,10 +22,21 @@ AT = AssocTypes
 LifeCycleStates = DotDict()
 LCS = LifeCycleStates
 
+lcs_workflows = {}
+
 def get_association_type_list():
     AssociationTypes.clear()
     AssociationTypes.update(Config(["res/config/associations.yml"]).data['AssociationTypes'])
     return AssociationTypes.keys()
+
+def initialize_res_lcsms():
+    """
+    Initializes default and special resource type state machines
+    @todo. Make dynamic later and maybe move out.
+    """
+    lcs_workflows.clear()
+    default_lcsm = ResourceLifeCycleSM()
+    lcs_workflows['Resource'] = default_lcsm
 
 def load_definitions():
     """Loads constants for resource, association and life cycle states.
@@ -44,27 +55,124 @@ def load_definitions():
     AssocTypes.update(zip(at_list, at_list))
 
     # Life cycle states
-    lcs_list = [
-        'NEW',              # Newly created, potentially inconsistent
-        'REGISTERED',       # Aggregate state: Consistent, ready for cross-referencing, not retired
-         'UNDEPLOYED',      # Aggregate state: Not deployed in target environment
-          'PLANNED',        # Consistent, with actual resource not yet present
-          'DEVELOPED',      # The actual resource exists
-          'TESTED',         # The actual resource is tested by provider
-          'INTEGRATED',     # The actual resource is integrated into composite and tested
-          'COMMISSIONED',   # The actual resource is certified commissioned
-
-         'DEPLOYED',        # Aggregate state: Not Deployed in target environment
-          'OFF',            # Aggregate state: Off
-          'ON',             # Aggregate state: On
-          'PUBLIC',         # Aggregate state: Announced, discoverable
-          'PRIVATE',        # Aggregate state: Unannounced, not discoverable
-           'OFFLINE',       # Inactive, unannounced
-           'ONLINE',        # Active, unannounced
-           'INACTIVE',      # Inactive, announced (public)
-           'ACTIVE',        # Active, announced (public)
-        'RETIRED',          # For historic reference only
-        ]
-    # ON, OFF, DEPLOYED, TESTED (various)
+    initialize_res_lcsms()
     LifeCycleStates.clear()
-    LifeCycleStates.update(zip(lcs_list, lcs_list))
+    LifeCycleStates.update(zip(ResourceLifeCycleSM.BASE_STATES, ResourceLifeCycleSM.BASE_STATES))
+
+def get_restype_lcsm(restype):
+    return lcs_workflows['Resource']
+
+class ResourceLifeCycleSM(object):
+    """
+    Base class for all resource type life cycle state workflows. Specific subtypes of resources can
+    add/remove transitions.
+    Supports hierarchical states
+    """
+
+    # Names of states and aggregate states
+    DRAFT =       'DRAFT'            # Newly created, potentially inconsistent
+    REGISTERED =  'REGISTERED'       # Aggregate state: Consistent, ready for cross-referencing, not retired
+    UNDEPLOYED =   'UNDEPLOYED'      # Aggregate state: Not deployed in target environment
+    PLANNED =       'PLANNED'        # Consistent, with actual resource not yet present
+    DEVELOPED =     'DEVELOPED'      # The actual resource exists
+    TESTED =        'TESTED'         # The actual resource is tested by provider
+    INTEGRATED =    'INTEGRATED'     # The actual resource is integrated into composite and tested
+    COMMISSIONED =  'COMMISSIONED'   # The actual resource is certified commissioned
+    DEPLOYED =     'DEPLOYED'        # Aggregate state: Not Deployed in target environment
+    OFF =           'OFF'            # Aggregate state: Off
+    ON =            'ON'             # Aggregate state: On
+    PUBLIC =        'PUBLIC'         # Aggregate state: Announced, discoverable
+    PRIVATE =       'PRIVATE'        # Aggregate state: Unannounced, not discoverable
+    OFFLINE =        'OFFLINE'       # Inactive, unannounced
+    ONLINE =         'ONLINE'        # Active, unannounced
+    INACTIVE =       'INACTIVE'      # Inactive, announced (public)
+    ACTIVE =         'ACTIVE'        # Active, announced (public)
+    RETIRED =     'RETIRED'          # Resource for historic reference only
+
+    BASE_STATES = [
+        DRAFT,
+        PLANNED, DEVELOPED, TESTED, INTEGRATED, COMMISSIONED,
+        OFFLINE, ONLINE, INACTIVE, ACTIVE,
+        RETIRED
+    ]
+
+    STATE_ALIASES = {
+        REGISTERED: (PLANNED, DEVELOPED, TESTED, INTEGRATED, COMMISSIONED,
+                     OFFLINE, ONLINE, INACTIVE, ACTIVE),
+        UNDEPLOYED: (PLANNED, DEVELOPED, TESTED, INTEGRATED, COMMISSIONED),
+        DEPLOYED: (OFFLINE, ONLINE, INACTIVE, ACTIVE),
+        OFF: (OFFLINE, INACTIVE),
+        ON: (ONLINE, ACTIVE),
+        PUBLIC: (INACTIVE, ACTIVE),
+        PRIVATE: (OFFLINE, ONLINE),
+    }
+
+    # Names of transition events
+    REGISTER = "register"
+    DEVELOP = "develop"
+    TEST = "test"
+    INTEGRATE = "integrate"
+    COMMISSION = "commission"
+    DECOMMISSION = "decommission"
+    DEPLOY = "deploy"
+    RECOVER = "recover"
+    ACTIVATE = "activate"
+    DEACTIVATE = "deactivate"
+    PUBLISH = "publish"
+    UNPUBLISH = "unpublish"
+    RETIRE = "retire"
+
+    BASE_TRANSITIONS = {
+        (DRAFT, REGISTER) : PLANNED,
+        (PLANNED, DEVELOP): DEVELOPED,
+        (DEVELOPED, TEST): TESTED,
+        (TESTED, INTEGRATE): INTEGRATED,
+        (INTEGRATED, COMMISSION): COMMISSIONED,
+        (COMMISSIONED, DECOMMISSION): TESTED,
+        (COMMISSIONED, DEPLOY): OFFLINE,
+        (OFFLINE, ACTIVATE): ONLINE,
+        (OFFLINE, PUBLISH): INACTIVE,
+        (INACTIVE, ACTIVATE): ACTIVE,
+        (INACTIVE, UNPUBLISH): OFFLINE,
+        (ONLINE, PUBLISH): ACTIVE,
+        (ONLINE, DEACTIVATE): OFFLINE,
+        (ACTIVE, UNPUBLISH): ONLINE,
+        (ACTIVE, DEACTIVATE): INACTIVE,
+        (REGISTERED, RETIRE): RETIRED,
+    }
+
+    def __init__(self):
+        self.transitions = {}
+        # Flatten transitions originating from hierarchical states
+        for (s0,ev),s1 in self.BASE_TRANSITIONS.iteritems():
+            assert s1 not in self.STATE_ALIASES, "Transition target state cannot be hierarchical"
+            if s0 in self.STATE_ALIASES:
+                for state in self.STATE_ALIASES[s0]:
+                    self.transitions[(state,ev)] = s1
+            else:
+                self.transitions[(s0,ev)] = s1
+        #import pprint
+        #pprint.pprint(self.transitions)
+
+    def _create_basic_transitions(self):
+        pass
+
+    def _add_constraints(self):
+        pass
+
+    @classmethod
+    def is_in_state(cls, current_state, query_state):
+        return (current_state == query_state) or (current_state in cls.STATE_ALIASES[query_state])
+
+    def get_successor(self, current_state, transition_event):
+        """
+        For given current_state and transition_event, return the successor state if
+        defined in the FSM transitions or None
+        """
+        return self.transitions.get((current_state, transition_event), None)
+
+
+class InformationResourceLCSM(ResourceLifeCycleSM):
+    ILLEGAL_STATES = []
+    ADD_TRANSITIONS = {
+    }
---
+++ pyon/ion/test/test_resource.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+__author__ = 'Michael Meisinger'
+
+from unittest import SkipTest
+
+
+from pyon.ion.resource import lcs_workflows, ResourceLifeCycleSM, LCS
+from pyon.util.unit_test import PyonTestCase
+from nose.plugins.attrib import attr
+
+@attr('UNIT', group='resource')
+class TestResources(PyonTestCase):
+
+    def test_resource_lcworkflow(self):
+        default_workflow = lcs_workflows['Resource']
+
+        self.assertEquals(len(ResourceLifeCycleSM.BASE_STATES), 11)
+        self.assertEquals(len(default_workflow.BASE_STATES), 11)
+        self.assertEquals(len(LCS), len(ResourceLifeCycleSM.BASE_STATES))
+
+        self.assert_(LCS.DRAFT in ResourceLifeCycleSM.BASE_STATES)
+
+        self.assert_(ResourceLifeCycleSM.is_in_state(LCS.DRAFT, LCS.DRAFT))
+        self.assert_(ResourceLifeCycleSM.is_in_state(LCS.DEVELOPED, ResourceLifeCycleSM.UNDEPLOYED))
+        self.assertFalse(ResourceLifeCycleSM.is_in_state(LCS.DEVELOPED, ResourceLifeCycleSM.DEPLOYED))
+        self.assert_(ResourceLifeCycleSM.is_in_state(LCS.DEVELOPED, ResourceLifeCycleSM.REGISTERED))
+
+        events = set(ev for (s0,ev) in ResourceLifeCycleSM.BASE_TRANSITIONS)
+        self.assertFalse(set(ResourceLifeCycleSM.BASE_STATES) & events)
+
+        self.assertEquals(len(default_workflow.transitions), 24)
+
+        self.assertEquals(default_workflow.get_successor(LCS.DRAFT, ResourceLifeCycleSM.REGISTER), LCS.PLANNED)
+
+        self.assertEquals(default_workflow.get_successor(LCS.PLANNED, ResourceLifeCycleSM.REGISTER), None)
+
+        self.assertEquals(default_workflow.get_successor(LCS.PLANNED, ResourceLifeCycleSM.DEVELOP), LCS.DEVELOPED)
+        self.assertEquals(default_workflow.get_successor(LCS.DEVELOPED, ResourceLifeCycleSM.RETIRE), LCS.RETIRED)
