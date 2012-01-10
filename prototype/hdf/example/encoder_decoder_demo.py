218f00e2172f5f88b6306cecc2fe876549a2e420
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-01-10 13:15:48 -0500

    Encoder and Decoder can now handle branching in group trees when the file has an existing tree. All unit tests pass successfully.

--- prototype/hdf/example/encoder_decoder_demo.py
+++ prototype/hdf/example/encoder_decoder_demo.py
@@ -2,17 +2,61 @@ import numpy, h5py
 
 from prototype.hdf.science_object_codec import HDFEncoder, HDFDecoder
 
-array = numpy.ones((4,5))
-dataset_name = 'rootgroup/mygroup/data/temperature'
+array1 = numpy.ones((4,5))
+array2 = numpy.ones((2,3))
+array3 = numpy.ones((10,2))
+dataset_name1 = 'rootgroup/mygroup/data/temperature'
+dataset_name2 = 'rootgroup/mygroup/data/pressure'
+dname = 'aGroup/adataset'
+
+###########################################################
 
 # Create an encoder object
 hdfencoder = HDFEncoder()
 # Add data as an array
-hdfencoder.add_hdf_dataset(dataset_name, array)
+hdfencoder.add_hdf_dataset(dataset_name1, array1)
+hdfencoder.add_hdf_dataset(dataset_name2, array2)
 # Convert all the data to a binary string for easy transportation
-hdfstring = hdfencoder.encoder_close()
+hdfstring1 = hdfencoder.encoder_close()
 
+# Create another encoder. This time pass on name of hdf5 file to write
+hdfencoder = HDFEncoder('/tmp/testHDFEncoder.hdf5')
+hdfencoder.add_hdf_dataset(dataset_name1, array1)
+hdfencoder.add_hdf_dataset(dataset_name2, array2)
+# Convert all the data to a binary string for easy transportation
+hdfstring2 = hdfencoder.encoder_close()
+
+# Create another encoder. This time pass on name of hdf5 file to write
+hdfencoder = HDFEncoder('/tmp/testHDFEncoder.hdf5')
+hdfencoder.add_hdf_dataset(dname, array3)
+# Convert all the data to a binary string for easy transportation
+hdfstring3 = hdfencoder.encoder_close()
+
+##########################################################
+
+print('Dataset: %s ' % dataset_name2)
 # Create a decoder object
-hdfdecoder = HDFDecoder(hdfstring)
+hdfdecoder = HDFDecoder(hdfstring1)
 # Read the array out of the decoder
-hdfdecoder.read_hdf_dataset(dataset_name)
+print hdfdecoder.read_hdf_dataset(dataset_name2)
+
+print('Dataset: %s ' % dataset_name1)
+# Create a decoder object
+hdfdecoder = HDFDecoder(hdfstring2)
+# Read the array out of the decoder
+print hdfdecoder.read_hdf_dataset(dataset_name1)
+
+#print "Third decoded hdf_string: "
+## Create a decoder object
+hdfdecoder = HDFDecoder(hdfstring3)
+# Read the array out of the decoder
+print('Dataset: %s ' % dataset_name1)
+print hdfdecoder.read_hdf_dataset(dataset_name1)
+
+hdfdecoder = HDFDecoder(hdfstring3)
+print("Dataset: %s" % dataset_name2)
+print hdfdecoder.read_hdf_dataset(dataset_name2)
+
+hdfdecoder = HDFDecoder(hdfstring3)
+print("Dataset: %s" % dname)
+print hdfdecoder.read_hdf_dataset(dname)
