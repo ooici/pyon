7de9e953eed1dbc75877331627a68b343095a2f8
David Stuebe <dstuebe@asascience.com>
2012-01-18 10:57:36 -0500

    Updated script in stream_consumer

--- examples/stream/stream_consumer.py
+++ examples/stream/stream_consumer.py
@@ -7,7 +7,7 @@ __author__ = 'Michael Meisinger'
 id = cc.spawn_process('myconsumer', 'examples.stream.stream_consumer', 'StreamConsumer', {'process':{'type':"stream_process",'listen_name':'a_queue'}})
 
 from examples.stream.stream_consumer import BindingChannel
-channel = cc.node.channel(BindingChannel, BindingChannel)
+channel = cc.node.channel(BindingChannel)
 channel.setup_listener(('science_data', 'a_queue'), binding='daves_special_sauce')
 #channel.start_consume()
 """
