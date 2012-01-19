33875aac783b1c74c33cbaf3b00cea44cdefbd5f
David Stuebe <dstuebe@asascience.com>
2012-01-19 18:31:32 -0500

    Imrpoved apis discussed with architecture team

--- prototype/hdf/constructor_apis.py
+++ prototype/hdf/constructor_apis.py
@@ -12,36 +12,46 @@
 
 
 
-class StationDatasetDefinition(object):
+class StationDataStreamDefinitionConstructor(object):
     """
-    A science object constructor for a station dataset.
+    A science object constructor for a station data stream. This constructor is the canonical way to build the metadata
+    object that defines the supplements published to the stream.
     """
 
     def __init__(self,):
         """
-        Instantiate a station dataset constructor. Create a new one or load an existing datastructure.
+        Instantiate a station dataset constructor.
         """
 
     @classmethod
-    def LoadStationDefinition(cls, StationDefMetadata):
-        pass
-
-
-    def define_variable(self, name, units, standard_name, dimensions):
+    def LoadStationDefinition(cls, metadata_object):
         """
-        @brief define a variable (quantity) present in the station dataset
-        @retval variable id
+        Load an existing data structure
         """
         pass
 
-    def add_attribute(self, subject_id, name=None, attribute=None):
-        pass
+    def define_reference_frame(self, temporal=None, geospatial=None):
+        """
+        define the reference frame for each and provide an absolute reference for each
+        """
 
-    def to_string(self, id):
+
+    def define_coverage(self, id=None, units=None, standard_name=None, coverage_dimensions=None):
         """
-        return a string containing the metadata for the particular object id for debug purposes.
+        @brief define a coverage (observed quantity) present in the station dataset
         """
-        pass
+
+    def add_nil_values(self, coverage_id=None, value=None, reason=None):
+        """
+        Add nil values to a particular coverage
+        """
+
+
+    def add_attribute(self, subject_id, id=None, value=None):
+        """
+        Create a new attribute for the subject by passing a new id and value
+        Add an existing attribute to the subject by passing just the subject_id and the id
+        """
 
     def __str__(self):
         """
@@ -56,28 +66,29 @@ class StationDatasetDefinition(object):
         pass
 
 
-class StationSupplement(object):
+class StationSupplementConstructor(object):
 
-    def __init__(self, station_definition=None):
-        pass
+    def __init__(self, station_definition=None, number_of_packets_in_record=None, packet_number_in_record=None):
+        """
+        @param station_definition is the metadata object defining the station record for this stream
+        """
 
 
-    def add_station(self, time=None, location=None):
+    def add_station(self, station_id=None, time=None, location=None):
         """
         Add a station to the dataset
-        @retval station id
         """
 
-    def add_station_variable(self, station_id, variable=None, values=None, slice=None):
+    def add_station_coverage(self, station_id=None, coverage_id=None, values=None, slice=None):
         """
-        Add data for a particular variable to a station. Slice represents the map to a know index structure for
+        Add data for a particular coverage to a station. Slice represents the map to a know index structure for
         n-dimensional data.
         """
 
-    def add_attribute(self, subject_id=None, name=None, attribute=None):
+    def add_attribute(self, subject_id, id=None, value=None):
         """
-        Add metadata to an object in the dataset
-        @retval annotation_id
+        Create a new attribute for the subject by passing a new id and value
+        Add an existing attribute to the subject by passing just the subject_id and the id
         """
 
     def _encode_supplement(self):
@@ -96,14 +107,81 @@ class Trajectory(object):
     A science object constructor for trajectory data.
     """
 
-class StructuredMeshDatasetDefinition(object):
+class StructuredMeshDataStreamDefinition(object):
     """
-    A science object constructor for mesh data.
+    A science object constructor for mesh data. This constructor is the canonical way to build the metadata
+    object that defines the supplements published to the stream.
     """
 
-    def define_mesh(self, name, x_coordinate=None, y_coordinate=None, z_coordinate=None ):
+    @classmethod
+    def LoadStructuredMeshDefinition(cls, metadata_object):
+        """
+        Load an existing data structure
+        """
+        pass
+
+    def define_reference_frame(self, temporal=None, geospatial=None):
+        """
+        define the reference frame for each and provide an absolute reference for each
+        """
+
+    def define_geospatial_domain(self, id=None, x_coordinate_values=None, y_coordinate_values=None, z_coordinate_values=None ):
         pass
 
 
-    def define_variable(self, name, units, standard_name, mesh_name, dimensions, location):
+    def define_coverage(self, id=None, units=None, standard_name=None, coverage_dimensions=None, mesh_location=None):
         pass
+
+    def add_nil_values(self, coverage_id=None, value=None, reason=None):
+        """
+        Add nil values to a particular coverage
+        """
+
+    def add_attribute(self, subject_id, id=None, value=None):
+        """
+        Create a new attribute for the subject by passing a new id and value
+        Add an existing attribute to the subject by passing just the subject_id and the id
+        """
+
+    def __str__(self):
+        """
+        Print the dataset metadata for debug purposes.
+        """
+        pass
+
+    def _encode_structure(self):
+        """
+        Method used to encode the station dataset metadata (structure)
+        """
+        pass
+
+
+
+class StructuredMeshSupplement(object):
+
+    def __init__(self, station_definition=None, number_of_packets_in_record=None, packet_number_in_record=None):
+        """
+        @param station_definition is the metadata object defining the station record for this stream
+        """
+
+    def add_temporal_domain(self, values=None):
+        """
+        """
+
+
+    def add_station_coverage(self, station_id=None, coverage_id=None, values=None, slice=None):
+        """
+        Add data for a particular coverage to a station. Slice represents the map to a know index structure for
+        n-dimensional data.
+        """
+
+    def add_attribute(self, subject_id, id=None, value=None):
+        """
+        Create a new attribute for the subject by passing a new id and value
+        Add an existing attribute to the subject by passing just the subject_id and the id
+        """
+
+    def _encode_supplement(self):
+        """
+        Method used to encode the station dataset supplement
+        """
