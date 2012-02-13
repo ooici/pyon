675cba9915036ffeccc021d644365d1c00255738
Dave Foster <dfoster@asascience.com>
2012-02-13 14:50:18 -0500

    Reimplement sync_call in transport layer

--- pyon/net/transport.py
+++ pyon/net/transport.py
@@ -4,16 +4,15 @@
 Transport layer abstractions
 
 TODOS:
-- port _sync_call from Channel layer for better error flow, add close callback to channel/client
 - split listen() into two subcalls (for StreamSubscriber)
 """
 
 __author__ = 'Dave Foster <dfoster@asascience.com>'
 __license__ = 'Apache 2.0'
 
-from pyon.util.async import blocking_cb
 from pyon.util.log import log
 from gevent.event import AsyncResult
+from contextlib import contextmanager
 
 class TransportError(StandardError):
     pass
@@ -46,9 +45,47 @@ class AMQPTransport(BaseTransport):
             cls.__instance = AMQPTransport()
         return cls.__instance
 
+    @contextmanager
+    def _push_close_cb(self, client, callback):
+        client.add_on_close_callback(callback)
+        try:
+            yield callback
+        finally:
+            # PIKA BUG: v0.9.5, we need to specify the callback as a dict - this is fixed in git HEAD (13 Feb 2012)
+            de = {'handle': callback, 'one_shot': True}
+            client.callbacks.remove(client.channel_number, '_on_channel_close', de)
+
+    def _sync_call(self, client, func, cb_arg, *args, **kwargs):
+        """
+        Functionally similar to the generic blocking_cb but with error support that's Channel specific.
+        """
+        ar = AsyncResult()
+
+        def cb(*args, **kwargs):
+            ret = list(args)
+            if len(kwargs): ret.append(kwargs)
+            ar.set(ret)
+
+        eb = lambda ch, *args: ar.set(TransportError("_sync_call could not complete due to an error (%s)" % args))
+
+        kwargs[cb_arg] = cb
+        with self._push_close_cb(client, eb):
+            func(*args, **kwargs)
+            ret_vals = ar.get()
+
+        if isinstance(ret_vals, TransportError):
+            raise ret_vals
+
+        if len(ret_vals) == 0:
+            return None
+        elif len(ret_vals) == 1:
+            return ret_vals[0]
+        return tuple(ret_vals)
+
+
     def declare_exchange_impl(self, client, exchange, exchange_type='topic', durable=False, auto_delete=True):
         log.debug("AMQPTransport.declare_exchange_impl: %s, T %s, D %s, AD %s", exchange, exchange_type, durable, auto_delete)
-        blocking_cb(client.exchange_declare, 'callback',
+        self._sync_call(client, client.exchange_declare, 'callback',
                                              exchange=exchange,
                                              type=exchange_type,
                                              durable=durable,
@@ -56,11 +93,11 @@ class AMQPTransport(BaseTransport):
 
     def delete_exchange_impl(self, client, exchange, **kwargs):
         log.debug("AMQPTransport.delete_exchange_impl: %s", exchange)
-        blocking_cb(client.exchange_delete, 'callback', exchange=exchange)
+        self._sync_call(client, client.exchange_delete, 'callback', exchange=exchange)
 
     def declare_queue_impl(self, client, queue, durable=False, auto_delete=True):
         log.debug("AMQPTransport.declare_queue_impl: %s, D %s, AD %s", queue, durable, auto_delete)
-        frame = blocking_cb(client.queue_declare, 'callback',
+        frame = self._sync_call(client, client.queue_declare, 'callback',
                                 queue=queue or '',
                                 auto_delete=auto_delete,
                                 durable=durable)
@@ -68,19 +105,18 @@ class AMQPTransport(BaseTransport):
 
     def delete_queue_impl(self, client, queue, **kwargs):
         log.debug("AMQPTransport.delete_queue_impl: %s", queue)
-        blocking_cb(client.queue_delete, 'callback', queue=queue)
+        self._sync_call(client, client.queue_delete, 'callback', queue=queue)
 
     def bind_impl(self, client, exchange, queue, binding):
         log.debug("AMQPTransport.bind_impl: EX %s, Q %s, B %s", exchange, queue, binding)
-        blocking_cb(client.queue_bind, 'callback',
+        self._sync_call(client, client.queue_bind, 'callback',
                                         queue=queue,
                                         exchange=exchange,
                                         routing_key=binding)
 
     def unbind_impl(self, client, exchange, queue, binding):
         log.debug("AMQPTransport.unbind_impl: EX %s, Q %s, B %s", exchange, queue, binding)
-        blocking_cb(client.queue_unbind, 'callback', queue=queue,
+        self._sync_call(client, client.queue_unbind, 'callback', queue=queue,
                                                      exchange=exchange,
                                                      routing_key=binding)
-
 
