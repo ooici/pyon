306c6fdc56958c9f330f65dee6a532219a32f59d
Luke Campbell <luke.s.campbell@gmail.com>
2012-02-20 12:13:43 -0500

    Luke: FileSystem
    - Fixed static vs instance vars problem.

--- pyon/util/file_sys.py
+++ pyon/util/file_sys.py
@@ -11,21 +11,22 @@ from pyon.core.bootstrap import CFG
 
 
 class FileSystem(object):
+    # These are static, and shared throughout the container, do not operate on a per-instance basis.
     FS_DIRECTORY_LIST = ['RESOURCE','TEMP','LIBRARY','CACHE','RUN','USERS','LOG']
     FS_DIRECTORY = DotDict(zip(FS_DIRECTORY_LIST,FS_DIRECTORY_LIST))
     def __init__(self):
-        for k,v in self.FS_DIRECTORY.iteritems():
+        for k,v in FileSystem.FS_DIRECTORY.iteritems():
             s = v.lower() # Lower case string
             conf = CFG.get_safe('system.filesystem.%s' % s, None)
             if conf:
-                self.FS_DIRECTORY[k] = conf
+                FileSystem.FS_DIRECTORY[k] = conf
             else:
-                self.FS_DIRECTORY[k] = os.path.join('/tmp',s)
+                FileSystem.FS_DIRECTORY[k] = os.path.join('/tmp',s)
 
             if not os.path.exists(self.FS_DIRECTORY[k]):
                 #@todo: Warning potential security problem if we use 777 for perms all the time
-                os.mkdir(self.FS_DIRECTORY[k], 0777)
+                os.mkdir(FileSystem.FS_DIRECTORY[k], 0777)
     def get_url(self, directory=''):
-        return self.FS_DIRECTORY.get_safe(directory)
+        return FileSystem.FS_DIRECTORY.get_safe(directory)
 
 FS_DIRECTORY = FileSystem.FS_DIRECTORY
