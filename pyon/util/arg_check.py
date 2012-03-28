f24883998d9b417514b9ffce9a90f8bce0f57923
Luke Campbell <luke.s.campbell@gmail.com>
2012-03-28 10:14:01 -0400

    Luke: Added documentation

--- pyon/util/arg_check.py
+++ pyon/util/arg_check.py
@@ -26,6 +26,11 @@ class ArgCheck(object):
 
 import_paths = [__name__]
 def scoped_assertion():
+    '''
+    Determines the calling module and line number.
+    Allows us to determine the file and line number where the assertion is made.
+    Inspired by pyon/util/log.py (Adam Smith)
+    '''
     frame = sys._getframe(1)
     name = frame.f_globals.get('__name__',None)
     lineno = frame.f_lineno
@@ -37,57 +42,142 @@ def scoped_assertion():
 
 
 def assertTrue(conditional, message='', exception=None):
+    '''
+    Manages an assertion
+    @param conditional The conditional statement to be evaluated
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if not conditional:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertEqual(a,b,message='',exception=None):
-    if not a==b:
+    '''
+    Raises an exception if a != b
+    @param a 
+    @param b
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
+    if a!=b:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertNotEqual(a,b,message='',exception=None):
+    '''
+    Raises an exception if a == b
+    @param a
+    @param b
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if a==b:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertFalse(conditional, message='', exception=None):
+    '''
+    Raises an exception if conditional evaluates to False
+    @param conditional The conditional statement to be evaluated
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if conditional:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertIs(a,b, message='', exception=None):
+    '''
+    Raises an exception if a does not points to the same object as b
+    @param a
+    @param b
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if not a is b:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertIsNot(a,b, message='', exception=None):
+    '''
+    Raises an exception if a points to the same object as b
+    @param a
+    @param b
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if a is b:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertIsNotNone(conditional, message='', exception=None):
+    '''
+    Raises an exception if conditional is None, does not point to anything and is not a number.
+    @param conditional The conditional statement to be evaluated
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if conditional is None:
 
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertIn(needle,haystack, message='', exception=None):
+    '''
+    Raises an exception if needle is not in haystack
+    @param needle Item to be evaluated for
+    @param haystack List or structure where the keyword 'in' can be used to evaluate a member exists within
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if not needle in haystack:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertNotIn(needle,haystack, message='', exception=None):
+    '''
+    Raises an exception if needle is in haystack
+    @param needle Item to be evaluated for
+    @param haystack List or structure where the keyword 'in' can be used to evaluate a member exists within
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if needle in haystack:
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertIsInstance(a,cls,message='',exception=None):
+    '''
+    Raises an exception if a is not an instance of cls
+    @param a Object
+    @param cls Class
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if not isinstance(a,cls):
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
 
 def assertNotIsInstance(a,cls, message='', exception=None):
+    '''
+    Raises an exception if a is an instance of cls
+    @param a Object
+    @param cls Class
+    @param message Error message to be included with the exception
+    @param Exception, exception module to use in lieu of default
+    @throws BadRequest if conditional is evaluated to False
+    '''
     if isinstance(a,cls):
         name,l = scoped_assertion()
         ArgCheck(name,exception).assertion(False,message,l)
