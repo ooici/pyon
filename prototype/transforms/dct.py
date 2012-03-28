ffaf7951c917c15ada65815531d5d8f81858f606
Luke Campbell <luke.s.campbell@gmail.com>
2012-03-28 15:21:37 -0400

    Luke: fixed dct

--- prototype/transforms/dct.py
+++ prototype/transforms/dct.py
@@ -20,19 +20,27 @@ class TransformDCT(TransformBenchTesting):
         assert isinstance(vector,np.ndarray)
         N = vector.size
 
-        x = np.zeros(N)
+        x = list()
         for k in xrange(N):
             if k == 0:
-                x[0] = np.sqrt(1./N) * np.sum(vector)
+                x.append( float(np.sqrt(1./N) * np.sum(vector)))
             else:
                 v = np.vectorize(lambda n : np.cos(np.pi * k *  (2. * n + 1.)  / (2. * N)))
-                x[k] = np.sqrt(2. / N) * np.sum(v(np.arange(0,N)) * vector)
+                x.append(float(np.sqrt(2. / N) * np.sum(v(np.arange(0,N)) * vector)))
         return x
 
     def process(self, packet):
+        import numpy as np
         validateIsInstance(packet,list)
         for i in packet:
-            validateIsInstance(packet[i],float)
-
-
+            validateIsInstance(i,float)
         self.publish(self.dct(np.array(packet)))
+'''
+from prototype.transforms.dct import TransformDCT
+def p(t):
+    print t
+
+t = TransformDCT()
+t.publish = p
+t.process([1.,2.,3.,4.])
+'''
