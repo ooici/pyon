03431a2a06c6dfa7c7c6f3784e9477689e1aa2d2
Jamie Chen <jachen@ucsd.edu>
2012-05-21 15:55:14 -0700

    Remove mock db code from int test

--- pyon/util/int_test.py
+++ pyon/util/int_test.py
@@ -39,13 +39,6 @@ class IonIntegrationTestCase(unittest.TestCase):
         self._turn_on_queue_auto_delete()
         self._patch_out_diediedie()
         self._patch_out_fail_fast_kill()
-        db_type = os.environ.get('DB_TYPE', None)
-        if not db_type:
-            pass
-        elif db_type == 'MOCK':
-            self._turn_on_mockdb()
-        elif db_type == 'COUCH':
-            self._turn_on_couchdb()
 
         # delete/create any known DBs with names matching our prefix - should be rare
         self._force_clean(True)
@@ -134,18 +127,6 @@ class IonIntegrationTestCase(unittest.TestCase):
             mod = servicecls.__module__
             cls = servicecls.__name__
         self.container.spawn_process(servicename, mod, cls, config)
-
-    def _turn_on_couchdb(self):
-        # Called via pyon.datastore.datastore.DataStoreManager.get_datastore()
-        patcher =patch('pyon.datastore.datastore.DatastoreManager.persistent', True)
-        patcher.start()
-        self.addCleanup(patcher.stop)
-
-    def _turn_on_mockdb(self):
-        # Called via pyon.datastore.datastore.DataStoreManager.get_datastore()
-        patcher =patch('pyon.datastore.datastore.DatastoreManager.persistent', False)
-        patcher.start()
-        self.addCleanup(patcher.stop)
 
     def _turn_off_force_clean(self):
         # Called via pyon.datastore.datastore.DataStoreManager.get_datastore()
