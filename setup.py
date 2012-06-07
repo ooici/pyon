6e175728aed89599866d3f698e1dc934a3b5e723
Jamie Chen <jachen@ucsd.edu>
2012-06-07 11:59:40 -0700

    Add test timer plug in

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(  name = 'pyon',
         entry_points = {
              'nose.plugins.0.10': [
                  'pycc_plugin=pyon.util.pycc_plugin:PYCC',
+                 'timer_plugin=pyon.util.timer_plugin:TestTimer',
                  'insulate=pyon.util.insulate:Insulate',
                  'insulateslave=pyon.util.insulate:InsulateSlave',
                  'gevent_profiler=pyon.util.nose_gevent_profiler:TestGeventProfiler'
