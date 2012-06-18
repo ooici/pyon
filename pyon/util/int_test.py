a9ff13f61a945e0d131b31f35f8d6bfb669ed167
Michael Meisinger <mmeisinger@ucsd.edu>
2012-06-18 13:32:52 -0700

    Fixed filesystem force clean issue

--- pyon/util/int_test.py
+++ pyon/util/int_test.py
@@ -42,7 +42,13 @@ class IonIntegrationTestCase(unittest.TestCase):
             da.load_datastore('res/dd')
 
         # hack to force_clean on filesystem
-        CFG['container']['filesystem'] = {}
+            # hack to force_clean on filesystem
+        try:
+            CFG['container']['filesystem']['force_clean'] = True
+        except KeyError:
+            CFG['container']['filesystem'] = {}
+            CFG['container']['filesystem']['force_clean'] = True
+
         self.container = None
         self.addCleanup(self._stop_container)
         self.container = Container()
