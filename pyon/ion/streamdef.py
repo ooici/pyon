ec63ac29ec17e4438124afcdb137f0f7e2a2bd4c
Luke Campbell <luke.s.campbell@gmail.com>
2012-07-05 17:09:38 -0400

    Luke: adds stream def comparison

--- pyon/ion/streamdef.py
+++ pyon/ion/streamdef.py
@@ -16,4 +16,22 @@ class StreamDef(object):
     def xpath(self, path):
         return self._element.xpath(path)
 
+    def __eq__(self, sd2):
+        return str(self) == str(sd2)
+
+    def __ne__(self, sd2):
+        return not self == sd2
+
+    def __repr__(self):
+        retval = str(self)
+        if len(retval) >= 14:
+            retval = retval[:14] + '...'
+        return '<%s "%s" at 0x%x>' % (self.__class__.__name__, retval, id(self))
+
+    def __str__(self):
+        return etree.tostring(self._element)
+
+    def __contains__(self, xpath):
+        return bool(self.xpath(xpath))
+
 
