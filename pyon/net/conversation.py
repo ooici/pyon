6c194400ea62c80a23f1d1adbefa846eff1faead
Rumyana Neykova <rumi.neykova@gmail.com>
2012-07-11 05:39:50 +0100

    Bug fixing

--- pyon/net/conversation.py
+++ pyon/net/conversation.py
@@ -7,7 +7,7 @@ from gevent import coros
 from pyon.core.exception import IonException
 from pyon.util.log import log
 from pyon.net.transport import NameTrio, BaseTransport
-from pyon.net.channel import BidirChannel, ChannelClosedError, ChannelShutdownMessage
+from pyon.net.channel import BidirChannel, ListenChannel, ChannelClosedError, ChannelShutdownMessage
 from pyon.core.interceptor.interceptor import Invocation, process_interceptors
 from gevent import queue as gqueue
 from gevent.event import AsyncResult
@@ -20,6 +20,7 @@ from pyon.core.bootstrap import CFG
 #@TODO: Add log.debug to all methods, can we do geenral on class level, except for typing it in every method?
 #@TODO: How to manage **kwargs???
 #@TODO: Add @param and @returns to all methods
+#@TODO: Queuest should be auto deleted
 
 class ConversationError(IonException):
     pass
@@ -48,16 +49,12 @@ MSG_TYPE_MASKS = enum(IN_SESSION = 7, CONTROL= 56)
 
 
 class Conversation(object):
-    _conv_table = {} # mapping between role and private channels
-    _conv_id = None
-    _protocol = None
-
-    """_buil_conv specific variables"""
     conv_id_counter = 0
     _lock = coros.RLock()       # @TODO: is this safe?
     _conv_id_root = None
 
     def __init__(self, protocol, cid = None):
+        self._conv_table = {}
         self.protocol = protocol
         if cid: self._conv_id = cid
         else:   self._conv_id = self._build_conv_id()
@@ -123,20 +120,15 @@ class Conversation(object):
 
 
 class ConversationEndpoint(object):
-    _invitation_table = {} # mapping between role and public channels (principals)
-    _is_originator = False
-    _self_role = None
-    _chan = None
-    _next_control_msg_type = 0
-    _recv_greenlet = None
-    _recv_queues = {}
-    _conv = None
-    inviter_role = None
-    node = None
 
     def __init__(self, node):
         log.debug("In Conversation.__init__")
         self.node = node
+        self._invitation_table = {} # mapping between role and public channels (principals)
+        self._is_originator = False
+        self._next_control_msg_type = 0
+        self._recv_queues = {}
+
 
     @property
     def conv(self):
@@ -183,7 +175,7 @@ class ConversationEndpoint(object):
         self._next_control_msg_type = 0
 
     #@TODO: Shell we add **kwargs???, why we need them
-    # @TODO: Pass User Headers
+    #@TODO: Pass User Headers
     def send(self, to_role, msg):
         if self._is_originator and not self._conv.has_role(to_role):
             _, is_invited  = self._invitation_table.get(to_role)
@@ -205,62 +197,62 @@ class ConversationEndpoint(object):
         return msg, header
 
     def close(self):
-        if self._recv_greenlet is not None:
-            # This is not entirely correct. We do it here because we want the listener's client_recv to exit gracefully
-            # and we may be reusing the channel. This *SEEMS* correct but we're reaching into Channel too far.
-            # @TODO: remove spawn_listener altogether.
-            self._chan._recv_queue.put(ChannelShutdownMessage())
-            self._recv_greenlet.join(timeout=2)
-            self._recv_greenlet.kill()      # he's dead, jim
-
-        if self._chan is not None:
+        if self._chan:
             # related to above, the close here would inject the ChannelShutdownMessage if we are NOT reusing.
             # we may end up having a duplicate, but I think logically it would never be a problem.
             # still, need to clean this up.
             self._chan.close()
+
+        if self._recv_greenlet:
+            # This is not entirely correct. We do it here because we want the listener's client_recv to exit gracefully
+            # and we may be reusing the channel. This *SEEMS* correct but we're reaching into Channel too far.
+            # @TODO: remove spawn_listener altogether.
+            print 'I am in spawn and will kill the greenlet'
+            self._chan._recv_queue.put(ChannelShutdownMessage())
+            self._recv_greenlet.join(timeout=1)
+            self._recv_greenlet.kill()      # he's dead, jim
 
     #@TODO: Do we need only one instance of a channel
     # We ahve preserved the logging from BaseEndpoint._spawn_listener
     def _spawn_listener(self, role, base_role_addr):
         def listen():
             print 'Conversation._spawn_listener'
-            role_addr = self._chan.setup_listener(NameTrio(base_role_addr))
+            recv_chan = self.node.channel(ListenChannel)
+            role_addr = recv_chan.setup_listener(NameTrio(base_role_addr))
             print 'Conversation.spawn_listener:role_addr: %s' %(role_addr)
             self._conv[role] = role_addr # this will block if there is no role in the _conv table, IS IT BAD???
-            self._chan.start_consume()
+            recv_chan.start_consume()
             while True:
                 try:
                     log.debug("ConversationEndpoint_listen waiting for a message")
-                    msg, header, delivery_tag =  self._chan.recv()
+                    msg, header, delivery_tag =  recv_chan.recv()
                     log.debug("ConversationEndpoint_listen got a message")
                     log_message(role_addr , msg, header, delivery_tag)
                     try:
                         self._on_msg_received(msg, header)
                     finally:
                         # always ack listener response
-                        self._chan.ack(delivery_tag)
+                        recv_chan.ack(delivery_tag)
                 except ChannelClosedError:
                     log.debug('Channel was closed during listen loop')
                     break
         self._recv_greenlet = spawn(listen)
 
-    def _on_msg_deliver_handler(self):
-        print 'in Conversation.on_msg_deliver_handler'
-
-
-
     def _on_msg_received(self, msg, header):
         print 'in Conversation._on_msg_received'
         control_msg_type = get_control_msg_type(header)
         in_session_msg_type = get_in_session_msg_type(header)
+        sender_role = header['sender-role']
         print 'Control msg type is:%s' %control_msg_type
         if control_msg_type == MSG_TYPE.ACCEPT or control_msg_type == MSG_TYPE.INVITE:
             print 'Inside Accept|Invite'
-            self._recv_queues.setdefault(header['sender-role'], gqueue.Queue())
-            # @TODO-Fix: reply-to should be renamed to sender-addr
-            self._conv[header['sender-role']] = NameTrio(tuple([x.strip() for x in header['reply-to'].split(',')]))
+            self._recv_queues.setdefault(sender_role, gqueue.Queue())
+            self._conv[sender_role] = NameTrio(tuple([x.strip() for x in header['reply-to'].split(',')]))
             if control_msg_type == MSG_TYPE.INVITE:
-                self.inviter_role = header['sender-role']
+                self.inviter_role = sender_role
+                #Note that if the message type is invite you need to accept it,
+                # conversation endpoint is an endpoint that is in a conevrsation. Only principals can accept/reject
+                #@ TODO: really need FSM mechanism here, optherwise it is so ugly :(
                 self._next_control_msg_type = MSG_TYPE.ACCEPT
         elif control_msg_type == MSG_TYPE.REJECT:
             exception_msg = 'Invitation rejected by role %s on address %s'\
@@ -269,7 +261,7 @@ class ConversationEndpoint(object):
             raise ConversationError(exception_msg)
 
         if in_session_msg_type == MSG_TYPE.TRANSMIT:
-            self._recv_queues[header['sender-role']].put((msg, header))
+            self._recv_queues[sender_role].put((msg, header))
 
     def _invite_and_send(self, to_role, msg, header = None, to_role_addr = None):
         log.debug("In _invite_and_send for msg: %s", msg)
@@ -343,7 +335,7 @@ class ConversationEndpoint(object):
 
         inv_prime = process_interceptors(interceptors["message_incoming"] if "message_incoming" in interceptors else [], inv)
 
-        return inv_prime.message, nv_prime.headers
+        return inv_prime.message, inv_prime.headers
 
     #Copy from endpoint.py, EndpointUnit
     def _intercept_msg_out(self, msg, header):
@@ -358,7 +350,7 @@ class ConversationEndpoint(object):
                     message=msg, headers=header)
         inv_prime = process_interceptors(interceptors["message_outgoing"] if "message_outgoing" in interceptors else [], inv)
 
-        return inv_prime.message, nv_prime.headers
+        return inv_prime.message, inv_prime.headers
 
     #Copy from endpoint.py, EndpointUnit
     def _build_invocation(self, **kwargs):
@@ -383,19 +375,16 @@ class ConversationEndpoint(object):
 
 
 class Principal(object):
-    # mapping between conv_id and conversation instance
-    _conversations = {}
-    _recv_queue = gqueue.Queue()
-    _recv_greenlet = None
-    _chan = None
     # keep the connection (AMQP)
     #@TODO: ensure node, may be by providing default broker connection
-    node = None
-    _name = None
 
     def __init__(self, node, name = None):
        self.node = node
        self._name = name
+       self._conversations = {}
+       self._recv_queue = gqueue.Queue()
+       self._chan = None
+       self._recv_greenlet = None
 
 
     @property
@@ -404,7 +393,7 @@ class Principal(object):
 
     @property
     def name(self):
-       if not instance(self._name, NameTrio):
+       if not isinstance(self._name, NameTrio):
            self._name = NameTrio(bootstrap.get_sys_name(), self._name)
        return self._name
 
@@ -414,9 +403,9 @@ class Principal(object):
     def spawn_listener(self, source_name = None):
        def listen():
            name = source_name or self.name
-           #@TODO: Should we have separate create_channel method?, do we need kwargs for creating a channel,
+           #@TODO: Should we have separated create_channel method?, do we need kwargs for creating a channel,
            # kwargs is normally set pass for the initialisation of the channel depending on the channel type: chan = ch_type(**kwargs)
-           self._chan = self.node.channel(BidirChannel)
+           self._chan = self.node.channel(ListenChannel)
            if name and isinstance(name, NameTrio):
                print 'In listen: before setup_listener'
                self._chan.setup_listener(name)
@@ -425,31 +414,34 @@ class Principal(object):
                raise PrincipalError('Principal.name is not correct: %s', name)
 
            self._chan.start_consume()
-           try:
-               with self._chan.accept() as newchan:
-                   print 'Before receiving invitation msg'
-                   msg, header, delivery_tag = newchan.recv()
-                   print 'After receiving invitation msg: %s, %s' %(msg, header)
-                   newchan.ack(delivery_tag)
-                   self._recv_invitation(msg, header)
-           except ChannelClosedError as ex:
-               log.debug('Channel was closed during LEF.listen')
+           while True:
+               try:
+                   with self._chan.accept() as newchan:
+                       print 'Before receiving invitation msg'
+                       msg, header, delivery_tag = newchan.recv()
+                       print 'After receiving invitation msg: %s, %s' %(msg, header)
+                       newchan.ack(delivery_tag)
+                       self._recv_invitation(msg, header)
+               except ChannelClosedError as ex:
+                   log.debug('Channel was closed during LEF.listen')
+                   break
        self._recv_greenlet = spawn(listen)
 
     def stop_listening(self):
-       if self._recv_greenlet is not None:
+        if self._chan:
+            # related to above, the close here would inject the ChannelShutdownMessage if we are NOT reusing.
+            # we may end up having a duplicate, but I think logically it would never be a problem.
+            # still, need to clean this up.
+            self._chan.close()
+
+        if self._recv_greenlet:
            # This is not entirely correct. We do it here because we want the listener's client_recv to exit gracefully
            # and we may be reusing the channel. This *SEEMS* correct but we're reaching into Channel too far.
            # @TODO: remove spawn_listener altogether.
            self._chan._recv_queue.put(ChannelShutdownMessage())
-           self._recv_greenlet.join(timeout=2)
+           self._recv_greenlet.join(timeout=1)
            self._recv_greenlet.kill()      # he's dead, jim
 
-       if self._chan is not None:
-           # related to above, the close here would inject the ChannelShutdownMessage if we are NOT reusing.
-           # we may end up having a duplicate, but I think logically it would never be a problem.
-           # still, need to clean this up.
-           self._chan.close()
 
     def get_invitation(self, protocol = None):
        # Here we should iterate while we find the protocol that is matched
@@ -468,7 +460,6 @@ class Principal(object):
     def _recv_invitation(self, msg, header):
        control_msg_type = get_control_msg_type(header)
        if control_msg_type == MSG_TYPE.INVITE:
-
            c = Conversation(header['protocol'], header['conv-id'])
            print '_accept_invitation: Conversation added to the list'
            #self._conversations[header['conv-id']] = c
@@ -509,13 +500,9 @@ class RPCClient(object):
     """
     Note: maybe we need RPCConversation and RPCOriginator, RPCPrincipal, they will return RPCClientEndpoint and RPCServerEndpoint
     """
-    _name = None
-    _node = None
-    _server_name = None
     # Will be nice to have
     # combine requestresponseClient.request and RequestEndpointUnit._send
-
-
+    s = 0
     def __init__(self, node, base_name, server_name):
         self.node = node
         self.name = base_name
@@ -533,19 +520,22 @@ class RPCClient(object):
         ts = time.time()
         local = ConversationOriginator(self.node, self.name)
         c = local.start_conversation('rpc', 'client')
-        c.invite('server', NameTrio('server', self._server_name))
-        c.send('server', 'Hello1')
+        c.invite('server', self.server_name)
+        c.send('server', 'Hello%s'%self.s)
         try:
-            result_data, result_headers = c.receive('server')
+            result_data, result_headers = c.recv('server')
         except Timeout:
             raise exception.Timeout('Request timed out (%d sec) waiting for response from %s' % (timeout, str(self.channel._send_name)))
         finally:
             elapsed = time.time() - ts
             log.info("Client-side request (conv id: %s/%s, dest: %s): %.2f elapsed", header.get('conv-id', 'NOCONVID'),
                 header.get('conv-seq', 'NOSEQ'),
-                self._server_addr,
+                self.server_name,
                 elapsed)
+            c.close()
+            local.stop_listening()
         log.debug("Response data: %s, headers: %s", result_data, result_headers)
+        self.s+=1
         return result_data, result_headers
 
     def buy_bonds(self, msg):
@@ -556,11 +546,9 @@ class RPCClient(object):
 # Again, headers are missing
 # This is indeed listener, and should be started in the process.py
 # create_errro_response and make_routing_call are still missing
+#@TODO; Implement. This is just a quick test, it is not a reasonable implementation of RPCServer
 #@TODO: Headers are not set and _service is not called
 class RPCServer(object):
-    node = None
-    name = None
-    _service = None
 
     def __init__(self, node, name, service = None):
         self.node = node
@@ -568,19 +556,23 @@ class RPCServer(object):
         self._service = service
 
     def listen(self):
-        local = Principal(self.node, self.name)
-        local.spawn_listener()
-        conv, msg, header  = local.get_invitation()
-        c = local.accept_invitation(conv, msg, header, auto_reply = 'True')
-        msg, header = c.recv('client')
-        self.process_msg()
-        c.send('client')
+            local = Principal(self.node, self.name)
+            local.spawn_listener()
+            while True:
+                conv, msg, header  = local.get_invitation()
+                c = local.accept_invitation(conv, msg, header, auto_reply = 'True')
+                msg, header = c.recv('client')
+                reply = self.process_msg(msg, header)
+                c.send('client', reply)
 
     def process_msg(self, msg, header):
-        if header['op'] == self._service:
-            print 'Correct call. The message is:%s' %(msg)
+        if header.setdefault('op', '') == self._service:
+            msg =  'Correct call. The message is:%s' %(msg)
+            print msg
         else:
-            print 'I do not support this call:%s' % (header['op'])
+            msg = 'I do not support this call:%s' % (header['op'])
+            print msg
+        return msg
 
 """
 So OOI specific RPCServer will hold a routing_object and will override process_msg to call the routing_obj (_service)
