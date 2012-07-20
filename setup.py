392d48e8a00fc4c8784e188aa6ded978ecbb7a46
Dave Foster <dfoster@asascience.com>
2012-07-20 15:13:43 -0400

    Release Version 0.1.5: Bugfix for 0.1.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 if sys.platform == 'darwin':
     os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
-version = '0.1.5-dev'
+version = '0.1.5'
 
 setup(  name = 'pyon',
         version = version,
