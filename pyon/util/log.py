128881a39bfeb53ed08d81afcb3571bcafdc1651
Luke Campbell <luke.s.campbell@gmail.com>
2012-08-08 10:34:52 -0400

    Luke: adds change_logging_level

--- pyon/util/log.py
+++ pyon/util/log.py
@@ -12,6 +12,16 @@ import sys
 import socket
 from logging.handlers import SysLogHandler, SYSLOG_UDP_PORT
 
+
+DEBUG     = 'DEBUG'
+INFO      = 'INFO'
+WARNING   = 'WARNING'
+ERROR     = 'ERROR'
+CRITICAL  = 'CRITICAL'
+EXCEPTION = 'EXCEPTION'
+
+
+
 class StackFormatter(logging.Formatter):
     def __init__(self,*a,**b):
         super(StackFormatter,self).__init__(*a,**b)
@@ -208,3 +218,10 @@ BaseConfigurator.importer = staticmethod(_import)
 
 log = get_scoped_log()
 
+def change_logging_level(level):
+    assert level in [DEBUG, INFO, WARNING, ERROR, CRITICAL]
+    from pyon.core.log import LOGGING_CFG
+    import logging.config
+    for k,v in LOGGING_CFG['loggers'].iteritems():
+        LOGGING_CFG['loggers'][k]['level'] = level
+    logging.config.dictConfig(LOGGING_CFG)
