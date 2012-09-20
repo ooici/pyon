3f9420d3130f269604035058c6a9e15b3330c978
Luke Campbell <luke.s.campbell@gmail.com>
2012-09-20 15:08:23 -0400

    Luke: removes deprecated transforms for bench test

--- prototype/transforms/linear.py
+++ prototype/transforms/linear.py
@@ -8,11 +8,6 @@
 from pyon.ion.transform import TransformBenchTesting
 from pyon.util.arg_check import validate_is_instance
 
-from pyon.util.log import log
-import numpy
-from pyon.ion.granule.record_dictionary import RecordDictionaryTool
-from pyon.ion.granule.taxonomy import TaxyTool
-from pyon.ion.granule.granule import build_granule, Granule
 
 
 class TransformLinear(TransformBenchTesting):
@@ -62,64 +57,22 @@ class TransformInPlace(TransformBenchTesting):
     '''
     Represents an algorithm of O(1)
     '''
-    @staticmethod
-    def shift(vector):
-        validate_is_instance(vector,list)
-        N = len(vector)
-        x = vector[0]
-        vector[0] = vector[N-1]
-        vector[N-1] = x
-        return vector
-
-    def process(self, packet):
-        if not isinstance(packet, list):
-            return
-        self.publish(self.shift(packet))
-
+    def __init__(self):
+        raise NotImplementedError('Deprecated')
 
 
 class TransformInPlaceNewGranule(TransformBenchTesting):
     '''
     Represents an algorithm of O(1)
     '''
-    @staticmethod
-    def shift(vector):
-        validate_is_instance(vector,numpy.ndarray)
-        x = vector[-1]
-        vector[1:] = vector[0:-1]
-        vector[0] = x
-        return vector
-
-    def process(self, packet):
-        if not isinstance(packet,Granule):
-            log.warn('Invalid packet received: Type "%s"' % type(packet))
-            return
-
-        rd_in = RecordDictionaryTool.load_from_granule(packet)
-        tt = TaxyTool.load_from_granule(packet)
-
-        rd_out = RecordDictionaryTool(tt)
-        for nickname, v_sequence in rd_in.iteritems():
-            rd_out[nickname] = self.shift(v_sequence)
-
-        g_out = build_granule(data_producer_id='dp_id',taxonomy=tt,record_dictionary=rd_out)
-        self.publish(g_out)
+    def __init__(self):
+        raise NotImplementedError('Deprecated')
 
 
 class TransformLinearNewGranule(TransformInPlaceNewGranule):
     '''
     Represents an algorithm of O(N)
     '''
-    @staticmethod
-    def shift(vector):
-        validate_is_instance(vector,numpy.ndarray)
-        N = len(vector)
-        x = numpy.zeros_like(vector)
-        for i in xrange(N):
-            if i == 0:
-                x[i] = vector[N-1]
-            else:
-                x[i] = vector[i-1]
-        return x
-
+    def __init__(self):
+        raise NotImplementedError('Deprecated')
 
