5527fb15a101f494cc6c7a64acd1602d5c9981bc
Prashant Kediyal <pkediyal@ucsd.edu>
2012-10-29 17:11:54 -0700

    fix PEP8 violations

--- pyon/ion/resregistry.py
+++ pyon/ion/resregistry.py
@@ -225,7 +225,7 @@ class ResourceRegistry(object):
         if attachment is None:
             raise BadRequest("Object not present")
         if not isinstance(attachment, Attachment):
-                raise BadRequest("Object is not an Attachment")
+            raise BadRequest("Object is not an Attachment")
 
         attachment.object_id = resource_id if resource_id else ""
 
@@ -362,7 +362,7 @@ class ResourceRegistry(object):
                            attr_name=None, attr_value=None, alt_id="", alt_id_ns="",
                            limit=None, skip=None, descending=None, id_only=True):
         return self.rr_store.find_resources_ext(restype=restype, lcstate=lcstate, name=name,
-                keyword=keyword, nested_type=nested_type,
+            keyword=keyword, nested_type=nested_type,
             attr_name=attr_name, attr_value=attr_value, alt_id=alt_id, alt_id_ns=alt_id_ns,
             limit=limit, skip=skip, descending=descending,
             id_only=id_only)
