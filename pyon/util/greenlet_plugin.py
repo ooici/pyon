2aefb400a0de9a44a4fc39e9498910c652d30e83
Dave Foster <dfoster@asascience.com>
2012-12-21 16:40:51 -0500

    Print greenlets to stderr/file on SIGUSR2
    With -n, output to stdout/stderr is blocked, so also dumps to a file ("gls-<pid>") on USR2.

--- pyon/util/greenlet_plugin.py
+++ pyon/util/greenlet_plugin.py
@@ -25,7 +25,8 @@ class GreenletLeak(Plugin):
     def begin(self):
         self._gls_by_test = {}
 
-    def _get_greenlets(self):
+    @classmethod
+    def get_greenlets(cls):
         """
         Gets a list of all greenlets the gc interface knows about.
 
@@ -34,16 +35,50 @@ class GreenletLeak(Plugin):
         return { obj for obj in gc.get_objects() if isinstance(obj, greenlet) and not obj.dead }
 
     def beforeTest(self, test):
-        self._pre_gls = self._get_greenlets()
+        self._pre_gls = self.get_greenlets()
 
     def afterTest(self, test):
-        post_gls = self._get_greenlets()
+        post_gls = self.get_greenlets()
 
         gls_added = post_gls.difference(self._pre_gls)
         gls_deleted = self._pre_gls.difference(post_gls)
 
         if len(gls_added) > 0:
             self._gls_by_test[test.id()] = gls_added
+
+    @classmethod
+    def format_greenlet(cls, gl):
+        """
+        Returns a tuple of [status/self/func] and stack if applicable.
+        """
+        if gl.dead:
+            return (["    (dead)"], [])
+        else:
+            selfstr = "(unknown)"
+            funcstr = "(unknown)"
+            if hasattr(gl, "_run"):
+                boundstr = "unbound"
+                if hasattr(gl._run, 'im_self'):
+                    selfstr = str(gl._run.im_self)
+                    boundstr = "bound"
+
+                try:
+                    if hasattr(gl._run, 'im_func'):
+                        funcstr = "%s, %s.%s" % (boundstr, gl._run.im_class.__name__, gl._run.im_func.__name__)
+                    elif hasattr(gl._run, 'func_name'):
+                        funcstr = "%s, %s" % (boundstr, func_name)
+
+                except Exception as ex:
+                    #print "WHAT HAPPEN", ex
+                    pass
+
+            selfstr = "self: " + selfstr
+            funcstr = "func: " + funcstr
+
+            # greenlet traceback (maybe interesting)
+            gltb = traceback.format_stack(gl.gr_frame)
+
+            return ([selfstr, funcstr], gltb)
 
     def report(self, stream):
         table = []
@@ -58,21 +93,11 @@ class GreenletLeak(Plugin):
             table.append([ptid, str(gls[0])])
 
             def append_gl_status(gl):
-                if gl.dead:
-                    table.append(["", "    (dead)"])
-                else:
-                    # self of method greenlet is running (hopefully __repr__ is defined)
-                    selfstr = "(none)"
-                    if hasattr(gl._run, 'im_self'):
-                        selfstr = str(gl._run.im_self)
-                    table.append(["", "self: " + selfstr])
-
-                    # greenlet traceback (maybe interesting)
-                    gltb = traceback.format_stack(gl.gr_frame)
-
-                    for line in gltb:
-                        for subline in line.split("\n")[0:2]:
-                            table.append(["", "   " + subline])
+                status = self.format_greenlet(gl)
+                table.append(["", "\n".join(status[0])])
+                for line in status[1]:
+                    for subline in line.split("\n")[0:2]:
+                        table.append(["", "   " + subline])
 
             append_gl_status(gls[0])
             table.append(["", ""])
