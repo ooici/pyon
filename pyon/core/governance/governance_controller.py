824ac4cd456a77a96991bde957814201670d3986
Stephen Henrie <shenrie@ucsd.edu>
2013-02-26 16:32:20 -0800

    Refactored governance module for easier use

--- pyon/core/governance/governance_controller.py
+++ pyon/core/governance/governance_controller.py
@@ -5,76 +5,16 @@ __author__ = 'Stephen P. Henrie'
 __license__ = 'Apache 2.0'
 
 import types
-from pyon.core.bootstrap import CFG, get_service_registry, IonObject
+
+from pyon.core.bootstrap import CFG, get_service_registry
 from pyon.core.governance.governance_dispatcher import GovernanceDispatcher
 from pyon.util.log import log
-from pyon.core.exception import BadRequest, Inconsistent
-from pyon.ion.resource import RT, PRED, LCS, OT
+from pyon.ion.resource import RT
 from pyon.core.governance.policy.policy_decision import PolicyDecisionPointManager
 from pyon.event.event import EventSubscriber
 from interface.services.coi.ipolicy_management_service import PolicyManagementServiceProcessClient
 from interface.services.coi.iresource_registry_service import ResourceRegistryServiceProcessClient
-from interface.services.coi.iorg_management_service import OrgManagementServiceProcessClient
 from pyon.core.exception import NotFound, Unauthorized
-from pyon.util.containers import get_ion_ts, get_safe
-
-#These constants are ubiquitous, so define in the container
-DEFAULT_ACTOR_ID = 'anonymous'
-ORG_MANAGER_ROLE = 'ORG_MANAGER'  # Can only act upon resource within the specific Org
-ORG_MEMBER_ROLE = 'ORG_MEMBER'    # Can only access resources within the specific Org
-ION_MANAGER = 'ION_MANAGER'   # Can act upon resources across all Orgs - like a Super User access
-
-
-class GovernanceHeaderValues(object):
-    '''
-    A helper class for containing governance values from a message header
-    '''
-
-    def __init__(self, headers, resource_id_required=True):
-        '''
-        A helper object for retrieving governance related values: op, actor_id, actor_roles, resource_id from the message header
-        @param headers:
-        @param resource_id_required: True if the message header must have a resource-id field and value.
-        @return op, actor_id, actor_roles, resource_id:
-        '''
-
-        if headers.has_key('op'):
-            self._op = headers['op']
-        else:
-            self._op = "Unknown operation"
-
-        if headers.has_key('ion-actor-id'):
-            self._actor_id = headers['ion-actor-id']
-        else:
-            raise Inconsistent('%s(%s) has been denied since the ion-actor-id can not be found in the message headers'% (self.name, op))
-
-        if headers.has_key('ion-actor-roles'):
-            self._actor_roles = headers['ion-actor-roles']
-        else:
-            raise Inconsistent('%s(%s) has been denied since the ion-actor-roles can not be found in the message headers'% (self.name, op))
-
-        if headers.has_key('resource-id'):
-            self._resource_id = headers['resource-id']
-        else:
-            if resource_id_required:
-                raise Inconsistent('%s(%s) has been denied since the resource-id can not be found in the message headers'% (self.name, op))
-            self._resource_id = ''
-
-    @property
-    def op(self):
-        return self._op
-
-    @property
-    def actor_id(self):
-        return self._actor_id
-
-    @property
-    def actor_roles(self):
-        return self._actor_roles
-
-    @property
-    def resource_id(self):
-        return self._resource_id
 
 
 class GovernanceController(object):
@@ -101,6 +41,8 @@ class GovernanceController(object):
     def start(self):
 
         log.debug("GovernanceController starting ...")
+
+        self._CFG = CFG
 
         self.enabled = CFG.get_safe('interceptor.interceptors.governance.config.enabled', False)
 
@@ -168,7 +110,28 @@ class GovernanceController(object):
             self.service_policy_event_subscriber.stop()
 
     @property
-    def _rr(self):
+    def is_container_org_boundary(self):
+        return self._is_container_org_boundary
+
+    @property
+    def container_org_name(self):
+        return self._container_org_name
+
+    @property
+    def system_root_org_name(self):
+        return self._system_root_org_name
+
+    @property
+    def is_root_org_container(self):
+        return self._is_root_org_container
+
+    @property
+    def CFG(self):
+        return self._CFG
+
+
+    @property
+    def rr(self):
         """
         Returns the active resource registry instance or client.
 
@@ -180,14 +143,6 @@ class GovernanceController(object):
 
         return self.rr_client
 
-    def is_container_org_boundary(self):
-        return self._is_container_org_boundary
-
-    def is_root_org_container(self):
-        return self._is_root_org_container
-
-    def get_container_org_boundary_name(self):
-        return self._container_org_name
 
     def get_container_org_boundary_id(self):
 
@@ -195,7 +150,7 @@ class GovernanceController(object):
             return None
 
         if self._container_org_id is None:
-            org, _ = self._rr.find_resources(restype=RT.Org,name=self._container_org_name)
+            org, _ = self.rr.find_resources(restype=RT.Org,name=self._container_org_name)
 
             if org:
                 self._container_org_id = org[0]._id
@@ -224,266 +179,6 @@ class GovernanceController(object):
 
         return invocation
 
-    #Helper methods
-
-    def get_role_message_headers(self, org_roles):
-        '''
-        Iterate the Org(s) that the user belongs to and create a header that lists only the role names per Org assigned
-        to the user; i.e. {'ION': ['Member', 'Operator'], 'Org2': ['Member']}
-        @param org_roles:
-        @return:
-        '''
-        role_header = dict()
-        try:
-            for org in org_roles:
-                role_header[org] = []
-                for role in org_roles[org]:
-                    role_header[org].append(role.name)
-            return role_header
-
-        except Exception, e:
-            log.error(e)
-            return role_header
-
-
-    def build_actor_header(self, actor_id=DEFAULT_ACTOR_ID, actor_roles=None):
-        '''
-        Use this to build the message header used by governance to identify the actor and roles.
-        @param actor_id:
-        @param actor_roles:
-        @return:
-        '''
-        actor_roles = actor_roles or {}
-        return {'ion-actor-id': actor_id, 'ion-actor-roles': actor_roles }
-
-    def get_actor_header(self, actor_id):
-        '''
-        Returns the actor related message headers for a specific actorid - will return anonymous if the actor_id is not found.
-        @param actor_id:
-        @return:
-        '''
-        actor_header = self.build_actor_header(DEFAULT_ACTOR_ID, {})
-
-        if actor_id:
-            try:
-                header_roles = self.find_roles_by_actor(actor_id)
-                actor_header = self.build_actor_header(actor_id, header_roles)
-            except Exception, e:
-                log.error(e)
-
-        return actor_header
-
-    def has_org_role(self, role_header=None, org_name=None, role_name=None):
-        '''
-        Check the ion-actor-roles message header to see if this actor has the specified role in the specified Org.
-        Parameter role_name can be a string with the name of a user role or a list of user role names, which will
-        recursively call this same method for each role name in the list until one is found or the list is exhausted.
-        @param role_header:
-        @param org_name:
-        @param role_name:
-        @return:
-        '''
-        if role_header is None or org_name is None or role_name is None:
-            raise BadRequest("One of the parameters to this method are not set")
-
-        if isinstance(role_name, list):
-            for role in role_name:
-                if self.has_org_role(role_header, org_name, role):
-                    return True
-        else:
-            if role_header.has_key(org_name):
-                if role_name in role_header[org_name]:
-                    return True
-
-        return False
-
-
-    def find_roles_by_actor(self, actor_id=None):
-        '''
-        Returns a dict of all User Roles roles by Org Name associated with the specified actor
-        @param actor_id:
-        @return:
-        '''
-        if actor_id is None or not len(actor_id):
-            raise BadRequest("The actor_id parameter is missing")
-
-        role_dict = dict()
-
-        role_list,_ = self._rr.find_objects(actor_id, PRED.hasRole, RT.UserRole)
-
-        for role in role_list:
-
-            if not role_dict.has_key(role.org_name):
-                role_dict[role.org_name] = list()
-
-            role_dict[role.org_name].append(role.name)
-
-        #Membership in ION Org is implied
-        if not role_dict.has_key(self._system_root_org_name):
-            role_dict[self._system_root_org_name] = list()
-
-        role_dict[self._system_root_org_name].append(ORG_MEMBER_ROLE)
-
-
-        return role_dict
-
-
-    def get_system_actor(self):
-        '''
-        Returns the ION System Actor defined in the Resource Registry
-        @return:
-        '''
-        try:
-            system_actor, _ = self._rr.find_resources(RT.ActorIdentity,name=get_safe(CFG, "system.system_actor", "ionsystem"), id_only=False)
-            if not system_actor:
-                return None
-
-            return system_actor[0]
-
-        except Exception, e:
-            log.error(e)
-            return None
-
-    def is_system_actor(self, actor_id):
-        '''
-        Is this the specified actor_id the system actor
-        @param actor_id:
-        @return:
-        '''
-        system_actor = self.get_system_actor()
-        if system_actor is not None and system_actor._id == actor_id:
-            return True
-
-        return False
-
-    def get_system_actor_header(self, system_actor=None):
-        '''
-        Returns the actor related message headers for a the ION System Actor
-        @param system_actor:
-        @return:
-        '''
-        try:
-            if system_actor is None:
-                system_actor = self.get_system_actor()
-
-            if not system_actor or system_actor is None:
-                log.warn('The ION System Actor Identity was not found; defaulting to anonymous actor')
-                actor_header = self.get_actor_header(None)
-            else:
-                actor_header = self.get_actor_header(system_actor._id)
-
-            return actor_header
-
-        except Exception, e:
-            log.error(e)
-            return self.get_actor_header(None)
-
-
-    def get_resource_commitments(self, actor_id, resource_id):
-        '''
-        Returns the list of commitments for the specified user and resource
-        @param actor_id:
-        @param resource_id:
-        @return:
-        '''
-        log.debug("Finding commitments for actor_id: %s and resource_id: %s" % (actor_id, resource_id))
-
-        try:
-
-            commitments,_ = self._rr.find_objects(resource_id, PRED.hasCommitment, RT.Commitment)
-            if not commitments:
-                return None
-
-            cur_time = int(get_ion_ts())
-            commitment_list = []
-            for com in commitments:  #TODO - update when Retired is removed from find_objects
-                if com.consumer == actor_id and com.lcstate != LCS.RETIRED and ( com.expiration == 0 or \
-                ( com.expiration > 0 and cur_time < com.expiration)):
-                    commitment_list.append(com)
-
-            if commitment_list:
-                return commitment_list
-
-        except Exception, e:
-            log.error(e)
-
-        return None
-
-    def has_resource_commitments(self, actor_id, resource_id):
-        '''
-        Returns a ResourceCommitmentStatus object indicating the commitment status between this resource/actor
-        Can only have an exclusive commitment if actor already has a shared commitment.
-        @param actor_id:
-        @param resource_id:
-        @return:
-        '''
-        ret_status = IonObject(OT.ResourceCommitmentStatus)
-        commitments = self.get_resource_commitments(actor_id, resource_id)
-        if commitments is None:
-            #No commitments were found between this resource_id and actor_id
-            return ret_status
-
-        ret_status.shared = True
-
-        for com in commitments:
-            if com.commitment.exclusive == True:
-                #Found an exclusive commitment
-                ret_status.exclusive = True
-                return ret_status
-
-        #Only a shared commitment was found
-        return ret_status
-
-
-    def has_shared_resource_commitment(self, actor_id=None, resource_id=None):
-        '''
-        This method returns True if the specified actor_id has acquired shared access for the specified resource id, otherwise False.
-        @param msg:
-        @param headers:
-        @return:
-        '''
-        if actor_id is None or resource_id is None:
-            raise BadRequest('One or all of the method parameters are not set')
-
-        commitment_status =  self.has_resource_commitments(actor_id, resource_id)
-
-        return commitment_status.shared
-
-
-    def has_exclusive_resource_commitment(self, actor_id=None, resource_id=None):
-        '''
-        This method returns True if the specified actor_id has acquired exclusive access for the specified resource id, otherwise False.
-        @param msg:
-        @param headers:
-        @return:
-        '''
-        if actor_id is None or resource_id is None:
-            raise BadRequest('One or all of the method parameters are not set')
-
-        commitment_status =  self.has_resource_commitments(actor_id, resource_id)
-
-        #If the resource has not been acquired for sharing, then it can't have been acquired exclusively
-        if not commitment_status.shared:
-            return False
-
-        return commitment_status.exclusive
-
-    def is_resource_owner(self, actor_id=None, resource_id=None):
-        '''
-        Returns True if the specified actor_id is an Owner of the specified resource id, otherwise False
-        @param actor_id:
-        @param resource_id:
-        @return:
-        '''
-        if actor_id is None or resource_id is None:
-            raise BadRequest('One or all of the method parameters are not set')
-
-        owners =  self._rr.find_objects(subject=resource_id, predicate=PRED.hasOwner, object_type=RT.ActorIdentity, id_only=True)
-
-        if actor_id not in owners[0]:
-            return False
-
-        return True
 
 
     #Manage all of the policies in the container
