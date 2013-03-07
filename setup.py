c454fca666e9e8fb894caed096557fcc1fb710d4
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-03-07 15:18:57 -0800

    update versions of utilities and ion-definitions to support container management events

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(  name = 'pyon',
         dependency_links = [
             'http://sddevrepo.oceanobservatories.org/releases/',
             'https://github.com/ooici/gevent-profiler/tarball/master#egg=python-gevent-profiler',
-            'https://github.com/ooici/utilities/tarball/v2012.12.12#egg=utilities-2012.12.12',
+            'https://github.com/ooici/utilities/tarball/v2013.03.07#egg=utilities-2012.03.07',
         ],
         test_suite = 'pyon',
         package_data = {'': ['*.xml']},
