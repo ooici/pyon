ce0491f5bb7d00b0ad2223b7905d2161a2d1d444
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-05-01 14:11:08 -0700

    bump utilities version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(  name = 'pyon',
         dependency_links = [
             'http://sddevrepo.oceanobservatories.org/releases/',
             'https://github.com/ooici/gevent-profiler/tarball/master#egg=python-gevent-profiler',
-            'https://github.com/ooici/utilities/tarball/v2013.03.29#egg=utilities-2013.03.29',
+            'https://github.com/ooici/utilities/tarball/v2013.05.01#egg=utilities-2013.05.01',
         ],
         test_suite = 'pyon',
         package_data = {'': ['*.xml']},
