e4c30c5b75a8f5c35c1df40b13e9d48d64fe5089
Jamie Chen <jachen@ucsd.edu>
2013-09-26 11:25:27 -0700

    Add children reporting to memory profile

--- pyon/util/memory_plugin.py
+++ pyon/util/memory_plugin.py
@@ -3,7 +3,7 @@ from resource import RUSAGE_SELF, RUSAGE_CHILDREN
 import psutil
 
 from nose.plugins.base import Plugin
-import sys
+import sys, os
 debug = sys.stderr
 
 class MemProfile(Plugin):
@@ -27,6 +27,8 @@ class MemProfile(Plugin):
         self.b_vm = None
         self.b_sru_maxrss = 0
         self.b_cru_maxrss = 0
+        self.proc = psutil.Process(os.getpid())
+        self.children = ""
 
     def startTest(self, test):
         self.b_vm = psutil.virtual_memory()
@@ -39,14 +41,19 @@ class MemProfile(Plugin):
         a_vm = psutil.virtual_memory()
         a_sru_maxrss = resource.getrusage(RUSAGE_SELF).ru_maxrss
         a_cru_maxrss = resource.getrusage(RUSAGE_CHILDREN).ru_maxrss
-        self._profiled_tests.append((test.id(), self.b_vm, self.b_sru_maxrss,
-            self.b_cru_maxrss, a_vm, a_sru_maxrss, a_cru_maxrss))
+        children = self.proc.get_children()
+        num_children = len(children)
+        child_names = [child.name for child in children]
+        self.children = "%d children: %s" % (num_children, ', '.join(child_names))
 
+        self._profiled_tests.append((test.id(), self.b_vm, self.b_sru_maxrss,
+            self.b_cru_maxrss, a_vm, a_sru_maxrss, a_cru_maxrss, self.children))
         debug.write("After: self maxrss=%s, tchildren maxrss=%s, %s\n" % (a_sru_maxrss, a_cru_maxrss, str(a_vm)))
+        debug.write("After: %s\n" % self.children)
 
     def report(self, stream):
         table = []
-        for test, b_vm, b_sru_maxrss, b_cru_maxrss, a_vm, a_sru_maxrss, a_cru_maxrss  \
+        for test, b_vm, b_sru_maxrss, b_cru_maxrss, a_vm, a_sru_maxrss, a_cru_maxrss, children  \
                 in self._profiled_tests:
             # printable tid: don't need the full path
             ptid = ".".join(test.split(".")[-2:])
@@ -58,10 +65,11 @@ class MemProfile(Plugin):
             table.append(["", "%s" % str(a_vm)])
             table.append(["", "Diff  : self maxrss=%s, children maxrss=%s" % (str(a_sru_maxrss - b_sru_maxrss),
                 str(a_cru_maxrss - b_cru_maxrss))])
+            table.append(["", "%s" % children])
             table.append(["", ""])
 
         # header
-        table.insert(0, ['Test', 'Memory Stats'])
+        table.insert(0, ['Test', 'Memory Stats: maxrss in KB(linux2) or Bytes(darwin), vmem in Bytes'])
 
         # get widths
         widths = [max([len(row[x]) for row in table]) for x in xrange(len(table[0]))]
