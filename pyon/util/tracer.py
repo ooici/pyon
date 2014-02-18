c956fa39c3740ff60bb87365aa6ba96a801b9b3f
Michael Meisinger <mmeisinger@ucsd.edu>
2014-02-18 15:55:21 -0800

    Add real-time trace logging to call tracer

--- pyon/util/tracer.py
+++ pyon/util/tracer.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
-"""General utility to trace important calls or events in the system"""
+"""General utility to trace important calls or events in the system, see
+https://confluence.oceanobservatories.org/display/CIDev/Collecting+and+managing+system+statistics+and+timing"""
 
 __author__ = 'Michael Meisinger'
 
 import inspect
 from collections import defaultdict
+# create special logging category for tracer logging
+import logging
+tracerlog = logging.getLogger('tracer')
 
 from pyon.util.containers import get_ion_ts, get_datetime_str
 
-from ooi.logging import log
+from ooi.logging import log, DEBUG
 
 DEFAULT_CONFIG = {"enabled": True,
                   "max_entries": 5000,
+                  "log_trace": False,
+                  "log_filter": "",
+                  "log_color": False,
+                  "log_stack": False,
+                  "log_truncate": 2000,
                   }
 
 # Global trace log data
@@ -48,31 +57,54 @@ class CallTracer(object):
 
     @staticmethod
     def log_scope_call(scope, log_entry, include_stack=True, stack_first_frame=4):
-        if not trace_data["config"].get("enabled", False):
+        try:
+            if not trace_data["config"].get("enabled", False):
+                return
+
+            log_entry["scope"] = scope
+            if not "ts" in log_entry:
+                log_entry["ts"] = get_ion_ts()
+            trace_data["scope_seq"][scope] += 1
+            log_entry["seq"] = trace_data["scope_seq"][scope]
+
+            if include_stack:
+                stack = inspect.stack()
+                frame_num = stack_first_frame
+                context = []
+                while len(stack) > frame_num and frame_num < 15:
+                    exec_line = "%s:%s:%s" % (stack[frame_num][1], stack[frame_num][2], stack[frame_num][3])
+                    context.insert(0, exec_line)
+                    if exec_line.endswith("_control_flow") or exec_line.endswith("load_ion") or exec_line.endswith("spawn_process")\
+                        or exec_line.endswith(":main") or exec_line.endswith(":dispatch_request"):
+                        break
+                    frame_num += 1
+                log_entry["stack"] = context
+
+            trace_data["trace_log"].append(log_entry)
+            if len(trace_data["trace_log"]) > trace_data["config"].get("max_entries", DEFAULT_CONFIG["max_entries"]) + 100:
+                trace_data["trace_log"] = trace_data["trace_log"][-trace_data["config"].get("max_entries", DEFAULT_CONFIG["max_entries"]):]
+
+            CallTracer.log_trace(log_entry)
+        except Exception as ex:
+            log.warn("Count not log trace call: %s", log_entry)
+
+    @staticmethod
+    def log_trace(log_entry):
+        if not trace_data["config"].get("log_trace", False):
             return
-
-        log_entry["scope"] = scope
-        if not "ts" in log_entry:
-            log_entry["ts"] = get_ion_ts()
-        trace_data["scope_seq"][scope] += 1
-        log_entry["seq"] = trace_data["scope_seq"][scope]
-
-        if include_stack:
-            stack = inspect.stack()
-            frame_num = stack_first_frame
-            context = []
-            while len(stack) > frame_num and frame_num < 15:
-                exec_line = "%s:%s:%s" % (stack[frame_num][1], stack[frame_num][2], stack[frame_num][3])
-                context.insert(0, exec_line)
-                if exec_line.endswith("_control_flow") or exec_line.endswith("load_ion") or exec_line.endswith("spawn_process")\
-                    or exec_line.endswith(":main") or exec_line.endswith(":dispatch_request"):
-                    break
-                frame_num += 1
-            log_entry["stack"] = context
-
-        trace_data["trace_log"].append(log_entry)
-        if len(trace_data["trace_log"]) > trace_data["config"].get("max_entries", DEFAULT_CONFIG["max_entries"]) + 100:
-            trace_data["trace_log"] = trace_data["trace_log"][-trace_data["config"].get("max_entries", DEFAULT_CONFIG["max_entries"]):]
+        if not tracerlog.isEnabledFor(DEBUG):
+            return
+        log_filter = trace_data["config"].get("log_filter", None) or ""
+        filter_scopes = log_filter.split(",")
+        log_stack = trace_data["config"].get("log_stack", None) or False
+        log_color = trace_data["config"].get("log_color", None) or False
+        log_truncate = trace_data["config"].get("log_truncate", None) or 2000
+        log_txt = CallTracer._get_log_text(log_entry, truncate=log_truncate, stack=log_stack, color=log_color)  # May change scope
+        logscope = log_entry["scope"]
+        scope_cat = logscope.split(".", 1)[0]
+        if not log_filter or logscope in filter_scopes or scope_cat in filter_scopes:
+            log_txt = log_txt.strip()
+            tracerlog.debug("%s", log_txt)
 
     @staticmethod
     def clear_scope(scope):
@@ -107,14 +139,9 @@ class CallTracer(object):
                 if scope and not logscope.startswith(scope):
                     continue
 
-                formatter = trace_data["format_cb"].get(logscope, None) or CallTracer._default_formatter
                 if tofile:
                     color = False
-                try:
-                    # Warning: Make sure formatter is reentrant. It may be called several times with the same entry
-                    log_txt = formatter(log_entry, truncate=truncate, stack=stack, color=color)
-                except Exception as ex:
-                    log_txt = "ERROR formatting: %s" % str(ex)
+                log_txt = CallTracer._get_log_text(log_entry, truncate=truncate, stack=stack, color=color)
                 logscope = log_entry["scope"]      # Read again to enable formatter to specialize scope
                 if scope and not logscope.startswith(scope):
                     continue
@@ -158,6 +185,17 @@ class CallTracer(object):
         finally:
             if f:
                 f.close()
+
+    @staticmethod
+    def _get_log_text(log_entry, truncate=2000, stack=False, color=False):
+        logscope = log_entry["scope"]
+        formatter = trace_data["format_cb"].get(logscope, None) or CallTracer._default_formatter
+        try:
+            # Warning: Make sure formatter is reentrant. It may be called several times with the same entry
+            log_txt = formatter(log_entry, truncate=truncate, stack=stack, color=color)
+        except Exception as ex:
+            log_txt = "ERROR formatting: %s" % str(ex)
+        return log_txt
 
     @staticmethod
     def _default_formatter(log_entry, **kwargs):
