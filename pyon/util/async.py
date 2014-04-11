29d65ad8407db037599cae202feea70c577bdb18
Luke Campbell <luke.s.campbell@gmail.com>
2014-04-11 13:28:36 -0400

    Adds asynchronous thread management
    - Posix Thread and Gevent support

--- pyon/util/async.py
+++ pyon/util/async.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__author__ = 'Adam R. Smith'
+__author__ = 'Adam R. Smith,Luke'
 
 import gevent
 from gevent.event import Event
@@ -61,4 +61,164 @@ def blocking_cb(func, cb_arg, *args, **kwargs):
     elif len(ret_vals) == 1:
         return ret_vals[0]
     return tuple(ret_vals)
+
+#--------------------------------------------------------------------------------
+
+import os
+import fcntl
+import gevent
+
+_pythread = None
+
+def get_pythread():
+    '''
+    Loads the thread module without monkey patching
+
+    source: https://github.com/nimbusproject/kazoo/blob/master/kazoo/sync/util.py
+    '''
+    global _pythread
+    if _pythread:
+        return _pythread # Cache the module so we don't have to use imp every time
+
+    import imp
+    fp, path, desc = imp.find_module('thread')
+    try:
+        _pythread = imp.load_module('pythread', fp, path, desc)
+    finally:
+        if fp:
+            fp.close() # Close the file
+    return _pythread
+
+class AsyncEvent(gevent.event.Event):
+    '''
+    A gevent-friendly event to be signaled by a posix thread and respected by 
+    the gevent context manager.
+
+
+    Essentially the posix thread signals gevent through a nonblocking pipe
+    '''
+
+    def __init__(self):
+        gevent.event.Event.__init__(self)
+        self._r, self._w = self._pipe() # non blocking pipe
+
+        # Create a new gevent core object that will observe the
+        # nonblocking pipe file descriptor for a value
+        self._core_event = gevent.core.event(
+                gevent.core.EV_READ | gevent.core.EV_PERSIST,
+                self._r,
+                self._pipe_read)
+        self._core_event.add()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._close()
+
+    def _pipe(self):
+        r, w  = os.pipe()
+        fcntl.fcntl(r, fcntl.F_SETFD, os.O_NONBLOCK)
+        fcntl.fcntl(w, fcntl.F_SETFD, os.O_NONBLOCK)
+        return r, w
+
+    def _pipe_read(self, event, eventtype):
+        '''
+        Non-blocking gevent friendly core event callback
+        http://www.gevent.org/gevent.core.html#events
+        '''
+        try:
+            os.read(event.fd, 1)
+        except EnvironmentError:
+            # file descriptors set with O_NONBLOCK return -1 and set errno to
+            # EAGAIN, we just want to ignore it and try again later
+            pass
+
+    def set(self):
+        '''
+        Sets the event value and writes a value to the pipe to
+        trigger the gevent core event
+        '''
+
+        gevent.event.Event.set(self)
+        os.write(self._w, '\0') # Empty C-string '\0'
+
+    def _close(self):
+        '''
+        Closes the core event, the read and write file descriptors
+        '''
+        if getattr(self, '_core_event', None):
+            try:
+                self._core_event.cancel()
+            except:
+                pass
+        if getattr(self, '_r', None):
+            try:
+                os.close(self._r)
+            except:
+                pass
+        if getattr(self, '_w', None):
+            try:
+                os.close(self._w)
+            except:
+                pass
+
+class AsyncDispatcher(object):
+    '''
+    Used to synchronize a result obtained in a pythread to a gevent thread
+    To use:
+
+    with AsyncDispatcher(callback, arg1, arg2, keyword=argument) as dispatcher:
+        v = dispatcher.wait(10)
+
+    The concurrency is NOT thread safe, do not attempt to modify shared regions inside the
+    dispatcher's context, it is solely meant to allow os threads to operate concurently
+    with greenlets.
+    '''
+    _value     = None
+    _exception = None
+    _set       = None
+    event      = None
+
+    def __init__(self, callback, *args, **kwargs):
+        self.callback = callback
+        self.args     = args
+        self.kwargs   = kwargs
+
+    def __enter__(self):
+        self.event = AsyncEvent()
+        self.event.__enter__()
+        pythread = get_pythread()
+        self._thread = pythread.start_new_thread(self.dispatch, (self.callback,) + self.args, self.kwargs)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.event.__exit__(type, value, traceback)
+
+    def dispatch(self, callback, *args, **kwargs):
+        '''
+        Runs a callback, either sets an asynchronous value or an exception
+        When the os thread completes, the event is set to signal gevent that it's complete
+        '''
+        try:
+            retval = callback(*args, **kwargs)
+            self._value = retval
+        except Exception as e:
+            self._exception = e
+        self.event.set()
+
+    def wait(self, timeout=None):
+        '''
+        Blocks the current gevent greenlet until a value is set or the timeout expires.
+        If the timeout expires a Timeout is raised.
+        If the callback raised an exception in the os thread, an exception is raised here.
+        '''
+        if self.event.wait(timeout):
+            if self._exception:
+                raise self._exception
+            else:
+                return self._value
+        else:
+            raise gevent.timeout.Timeout(timeout)
+
 
--- pyon/util/test/test_async.py
+++ pyon/util/test/test_async.py
@@ -2,9 +2,12 @@
 
 __author__ = 'Adam R. Smith'
 __license__ = 'Apache 2.0'
+import gevent
+import time
 
-from pyon.util.async import blocking_cb
+from pyon.util.async import blocking_cb, AsyncDispatcher
 from pyon.util.int_test import IonIntegrationTestCase
+from pyon.util.unit_test import PyonTestCase
 from nose.plugins.attrib import attr
 
 @attr('UNIT')
@@ -15,3 +18,50 @@ class AsyncTest(IonIntegrationTestCase):
     def test_blocking(self):
         a, b, c, misc = blocking_cb(self.i_call_callbacks, cb_arg='cb')
         self.assertEqual((a, b, c, misc), (1, 2, 3, {'foo': 'bar'}))
+
+
+@attr('UNIT')
+class TestThreads(PyonTestCase):
+
+    def load_clib(self):
+        from ctypes import cdll, util
+        try:
+            clib = cdll.LoadLibrary(util.find_library('c'))
+        except OSError as e:
+            if 'image not found' in e.message:
+                clib = cdll.LoadLibrary('libc.dylib') # The mac edge case
+            else:
+                raise
+        return clib
+
+    def clib_timeout(self):
+        clib = self.load_clib()
+        clib.sleep(5)
+
+    def test_true_block(self):
+        t0 = time.time()
+        self.clib_timeout()
+        t1 = time.time()
+
+        self.assertTrue((t1 - t0) >= 5)
+
+        t0 = time.time()
+        g = gevent.spawn(self.clib_timeout)
+        gevent.sleep(5)
+        g.join()
+        t1 = time.time()
+        # If it was concurrent delta-t will be less than 10
+        self.assertTrue((t1 - t0) >= 10)
+
+        # Syncing with gevent
+        t0 = time.time()
+        with AsyncDispatcher(self.clib_timeout) as dispatcher:
+            gevent.sleep(5)
+            dispatcher.wait(10)
+        t1 = time.time()
+
+        # Proof that they run concurrently and the clib call doesn't block gevent
+        self.assertTrue((t1 - t0) < 6)
+
+
+        
