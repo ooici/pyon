cf9111287f18db587266e9b974d3b0c5fc4a0f63
Luke Campbell <luke.s.campbell@gmail.com>
2014-04-14 16:46:43 -0400

    Primitive thread-pool-ish thing

--- pyon/util/async.py
+++ pyon/util/async.py
@@ -8,6 +8,7 @@ import fcntl
 from gevent.event import Event
 from collections import Iterable
 from functools import wraps
+import time
 
 
 spawn = gevent.spawn
@@ -71,9 +72,7 @@ _pythread = None
 
 def get_pythread():
     '''
-    Loads the thread module without monkey patching
-
-    source: https://github.com/nimbusproject/kazoo/blob/master/kazoo/sync/util.py
+    Loads the 'thread' module, free of monkey patching.
     '''
     global _pythread
     if _pythread:
@@ -87,6 +86,23 @@ def get_pythread():
         if fp:
             fp.close() # Close the file
     return _pythread
+
+
+_pytime = None
+def get_pytime():
+    global _pytime
+    if _pytime:
+        return _pytime
+
+    import imp
+    fp, path, desc = imp.find_module('time')
+    try:
+        _pytime = imp.load_module('pytime', fp, path, desc)
+    finally:
+        if fp:
+            fp.close()
+    return _pytime
+
 
 def nonblock_pipe():
     r, w = os.pipe()
@@ -199,6 +215,112 @@ class AsyncEvent(gevent.event.Event):
                 os.close(self._w)
             except:
                 pass
+
+class AsyncQueue(object):
+    '''
+    An extremely primitive queue that has a mutex over pthreads
+    '''
+    def __init__(self):
+        self.__queue = []
+        pythread = get_pythread()
+        self.lock = pythread.allocate_lock()
+
+    def put(self, item):
+        with self.lock:
+            self.__queue.append(item)
+
+    def get(self):
+        retval = None
+        with self.lock:
+            if self.__queue:
+                retval = self.__queue.pop(0)
+        return retval
+
+class AsyncTask(object):
+    def __init__(self, callback, *args, **kwargs):
+        self.ar = AsyncResult()
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
+
+
+class ThreadExit(Exception):
+    pass
+
+class ThreadJob(object):
+    def __init__(self, queue):
+        self.queue = queue
+
+    def sleep(self, n):
+        '''
+        Sleeps n seconds using the un-patched time module
+        '''
+        pytime = get_pytime()
+        pytime.sleep(n)
+
+    def run(self):
+        while True:
+            entry = self.queue.get()
+            if not entry:
+                self.sleep(0.02)
+                continue
+            elif isinstance(entry, ThreadExit):
+                break
+            elif not isinstance(entry, AsyncTask):
+                raise Exception("Invalid task")
+            ar = entry.ar
+            callback = entry.callback
+            args = entry.args
+            kwargs = entry.kwargs
+            try:
+                retval = callback(*args, **kwargs)
+                ar.set(retval)
+            except Exception as e:
+                from traceback import print_exc
+                print_exc()
+                ar.set_exception(e)
+        print "Exiting thread"
+
+class ThreadPool(object):
+    def __init__(self, poolsize=5):
+        self.poolsize = poolsize
+        self.queue = AsyncQueue()
+        self._spawn_threads(self.poolsize)
+
+    def _spawn_threads(self, num):
+        pythread = get_pythread()
+        for i in xrange(num):
+            job_worker = ThreadJob(self.queue)
+            pythread.start_new_thread(job_worker.run, tuple())
+
+    def resize(self, newsize):
+        pass
+
+    def apply_async(self, func, *args, **kwargs):
+        # TODO Pool still active?
+        task = AsyncTask(func, *args, **kwargs)
+        self.queue.put(task)
+        return task.ar
+
+    def apply(self, func, *args, **kwargs):
+        pass
+
+    def close(self):
+        for i in xrange(self.poolsize):
+            self.queue.put(ThreadExit())
+
+    def map(self, func, arg_list, abort=False):
+        pass
+
+    def map_async(self, func, arg_list):
+        pass
+
+    def queue_remove(self, async_res):
+        pass
+
+    def queue_remove_empty(self, async_res_list):
+        pass
+
 
 class AsyncDispatcher(object):
     '''
